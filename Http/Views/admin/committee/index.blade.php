35fb3b038717bac7bd8df97b1ca47e0d6cb89529
mark-boer <m.h.boer.2@gmail.com>
2016-02-25 02:49:42 +0100

    add committee tests

--- Http/Views/admin/committee/index.blade.php
+++ Http/Views/admin/committee/index.blade.php
@@ -28,21 +28,21 @@
     {!! csrf_field() !!}
 
     <div class="form-group">
-      <label class="col-sm-2 control-label">Committee name</label>
-      <div class="col-sm-10">
+      <label class="col-sm-4 control-label">Committee name</label>
+      <div class="col-sm-8">
         <input type="text" class="form-control" name="inputName">
       </div>
     </div>
 
     <div class="form-group">
-      <label class="col-sm-2 control-label">Committee goal</label>
-      <div class="col-sm-10">
+      <label class="col-sm-4 control-label">Committee goal</label>
+      <div class="col-sm-8">
         <input type="text" class="form-control" name="inputGoal">
       </div>
     </div>
 
     <div class="form-group">
-      <div class="col-sm-offset-2 col-sm-10">
+      <div class="col-sm-offset-4 col-sm-8">
         <button type="submit" class="btn btn-default">Create committee</button>
       </div>
     </div>
