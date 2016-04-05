76a0be0d5d661521dd34be54b105d180a8ce5fa3
MrTomBosma <tombosma.tb@gmail.com>
2016-04-05 20:31:59 +0200

    Starting front page design from ground up
    Slow progress, but I like it all the same.

--- src/Infrastructure/Http/Views/main.blade.php
+++ src/Infrastructure/Http/Views/main.blade.php
@@ -3,36 +3,50 @@
 
 @section('content')
 <div class="row">
-  <div class="col-md-7">
+  <!--<div class="col-md-7">
     <h1>Control Engineering, the new next thing</h1>
     <p>A bit more than a year after my previous blog, it is time for an update. Since this is my second blog, following the 1-2 tradition rule, I now have a steady series going on. Since every series has a common thread, this one can’t be missing out. With these blogs I want to share my experiences as a PhD-student and give you an insight in the science which is conducted at the other end of the building. In this series I will introduce you to a field which you won’t encounter that often as a physicist. </p>
     <p>Last time I wrote, I was at the precipice of my PhD-research, now I am already one year into my adventure. The first conclusion I can make is that time has not stood still. In a desperate attempt to remove all connections with business and management, the institute for which I work has changed names: from ITM (Institute for Technology and Management) to ENTEG (ENgineering and TEchnology institute Groningen). Whether or not this has worked, I do not know. What I do know however is that TBK (or IEM) is still here. But let’s not dwell on these minor matters.</p>
     <p><a href="#">Read more</a></p>
+  </div>-->
+
+
+
+<div class="col-sm-8">
+   <table class = "table table-hover">
+
+		  <h3>News</h3>
+	  
+      <tbody>       
+         <tr>
+            <td><img src="http://www.faketattoo.nl/uploads/big_sterretje5-68-1290096744.jpg" style="width:20px;height:20px;"></td>
+            <td><a href="#">Nieuwe commissie: De Huppeldepupcie</a></td>
+            <td>5 April 2016 20:12</td>
+         </tr>
+         
+         <tr>
+			<td><img src="https://image.freepik.com/free-icon/election-event-on-a-calendar-with-star-symbol_318-64485.png" style="width:20px;height:20px;">
+			<td><a href="#">Taart voor iedereen</a></td>
+			<td>4 April 2016 15:10</td>
+         </tr>
+         
+         <tr>
+            <td><img src="http://www.iamhrsinclair.com/wp-content/uploads/2013/02/universal-blog-icon-by-Southpaw.png" style="width:20px;height:20px;"></td>
+            <td><a href="#">Blog van Harry</a></td>
+            <td>4 April 2016 15:10</td>
+         </tr>
+      </tbody>
+      
+   </table>
+</div>  
+<div class="col-sm-4"><!-- Section with calendar of upcoming events = Dynamic-->
+	<h3>Upcoming events</h3>
   </div>
- 
-  <div class="col-md-5">
-    <h2>Upcoming events</h2>
-    <div class="panel panel-default">
-      <div class="panel-heading">Evenement <span class="date">12-09-1989<span></div>
-      <div class="panel-body">
-        Omschrijving evemenet
-      </div>
-    </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">Evenement <span class="date">12-09-1989<span></div>
-      <div class="panel-body">
-        Omschrijving evemenet
-      </div>
-    </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">Evenement <span class="date">12-09-1989<span></div>
-      <div class="panel-body">
-        Omschrijving evemenet
-      </div>
-    </div>
-  </div>
+  
+  
 </div><!--  .row  -->
 
+<!--
 <h3>Meet the board</h3>
 <div class="row">
   <div class="col-xs-6 col-md-3">
@@ -56,5 +70,6 @@
     </a>
   </div>
 </div>
-
+-->
+		
 @endsection
