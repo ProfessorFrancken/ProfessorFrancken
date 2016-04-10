1d3d02b4f7427072b624b359f17c3bb854364bde
Mark Redeman <markredeman@gmail.com>
2016-04-10 22:00:19 +0200

    Apply PSR2 code style guidelines

--- Francken/Members/Email.php
+++ Francken/Members/Email.php
@@ -4,15 +4,16 @@ namespace Francken\Members;
 
 final class Email
 {
-	private $email;
+    private $email;
 
-	public function __construct(string $email) {
-		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			throw new \InvalidArgumentException('"' . $email . '" is not a valid email');
-		}
+    public function __construct(string $email)
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException('"' . $email . '" is not a valid email');
+        }
 
-		$this->email = $email;
-	}
+        $this->email = $email;
+    }
 
     public function __toString() : string
     {
