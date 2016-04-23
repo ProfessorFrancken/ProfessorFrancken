aba3739ff64d047e5b923c77d2d73d16e27ba7e7
mark[1] <m.h.boer.2@gmail.com>
2016-04-23 22:57:05 +0200

    add book helper classes

---
+++ src/Domain/Books/Guest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Francken\Domain\Books;
+
+use Francken\Members\Email;
+
+class Guest implements SerializableInterface
+{
+	use Serializable;
+
+	private $name;
+	private $email;
+
+	public function __construct(string $name, Email $email)
+	{
+		$this->name = $name;
+		$this->email = $email;
+	}
+
+	public function name() : string
+	{
+		return $this->name;
+	}
+
+	public function email() : Email
+	{
+		return $this->email;
+	}
+}
---
+++ src/Domain/Books/ISBN.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Francken\Domain\Books;
+
+
+//isbn-10 without hyphens
+class ISBN
+{
+	private $isbn;
+
+	private function __construct(string $isbn)
+	{
+		$this->isbn = $isbn;
+	}
+
+	public static function fromString(string $isbn) : ISBN
+	{
+		if (!isValid($isbn))
+			throw new Exception("Invalid ISBN provided") 
+		return new ISBN($str);	
+	}
+
+	public function __toString()
+	{
+		return $this->isbn;
+	}
+
+	private function isValid(string $isbn) : bool
+	{
+		if(length($isbn) !== 10)
+			return false;
+		$sum = 0;
+		for($i = 0; i < 10; ++i)
+		{
+			$sum += ((int) $isbn[i]) * (9 - i);
+		}
+		if ($sum % 11 === 0)
+			return true;
+		return false;
+	}
+}
