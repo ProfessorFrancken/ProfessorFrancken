ab9ccbd2f5f4367ec574b44863849f6e8815b150
Mark Redeman <markredeman@gmail.com>
2016-09-04 18:40:59 +0200

    Implement basic job openings page

---
+++ src/Infrastructure/Http/Views/pages/career/_job-openings-of-company.blade.php
@@ -0,0 +1,15 @@
+<section class="job-opening">
+    <div class="col-md-9">
+        <h2 class="job-opening__company-name">{{ $company['name'] }}</h2>
+        <ul>
+            @foreach ($company['job-openings'] as $job)
+                <li>
+                    <a href="{{ $job['link'] }}">{{ $job['job'] }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="col-md-3">
+        <img class="job-opening__company-logo" alt="{{ $company['name'] }}" src="{{ $company['logo'] }}"/>
+    </div>
+</section>
