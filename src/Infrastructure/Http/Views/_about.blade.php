0732c3cc4b4c8250bd5084b3990e7ff7211ebcd0
Mark Redeman <markredeman@gmail.com>
2016-09-23 11:59:48 +0200

    More consistent footers

--- src/Infrastructure/Http/Views/_about.blade.php
+++ src/Infrastructure/Http/Views/_about.blade.php
@@ -1,13 +1,13 @@
 <div class="about-francken container-fluid">
     <div class="row">
-        <div class="study col-sm-3">
+        <div class="study col-sm-3 col-sm-offset-1">
             <h3>Study</h3>
             <p>
                 Nulla facilisi cras fermentum, odio eu feugiat pretium, nibh ipsum consequat nisl, vel pretium lectus quam id leo in vitae turpis massa sed elementum tempus. Massa placerat duis ultricies lacus.
             </p>
 
         </div>
-        <div class="association col-sm-3 col-sm-offset-1">
+        <div class="association col-sm-3">
             <h3>Association</h3>
             <p>
                 Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci sagittis eu volutpat! Id diam maecenas ultricies mi?
