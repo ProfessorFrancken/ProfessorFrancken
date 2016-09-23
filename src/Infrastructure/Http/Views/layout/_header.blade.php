2e405cbbf9124ee00428b427b64622d2f03ccab5
Mark Redeman <markredeman@gmail.com>
2016-09-23 11:18:43 +0200

    Use colors from Vrancken Vrij 20

--- src/Infrastructure/Http/Views/layout/_header.blade.php
+++ src/Infrastructure/Http/Views/layout/_header.blade.php
@@ -1,8 +1,7 @@
 <!-- Header -->
 <!-- background-image: url('http://www.professorfrancken.nl/wordpress/wp-content/uploads/2013/11/das_header-1080x400.png') -->
-    <header style="background-color: #31255E; margin-bottom: 1em; border-bottom: 25px solid #483A78;
-">
-    <div style="width:100%; height: 400px; color: white;">
-        <h1 style="padding-top: 160px; margin: 0;text-align: center;  font-size: 50px">T.F.V. 'Professor Francken'</h1>
+    <header class="front-header">
+    <div class="front-header__internal">
+        <h1 class="front-header__header">T.F.V. 'Professor Francken'</h1>
     </div>
 </header>
