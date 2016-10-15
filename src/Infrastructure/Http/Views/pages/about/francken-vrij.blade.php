68d9a28dfa63b0c0484c95325647263043782a39
Mark Redeman <markredeman@gmail.com>
2016-10-15 12:14:54 +0200

    Add link to Francken Vrij

--- src/Infrastructure/Http/Views/pages/about/francken-vrij.blade.php
+++ src/Infrastructure/Http/Views/pages/about/francken-vrij.blade.php
@@ -6,7 +6,7 @@
 
   <p>Francken Vrij is the popular science magazine of the Technisch Fysische Vereniging ‘Professor Francken’ and is distributed to its members, sponsors and other interested parties. The complete Francken Vrij archive can be found on this page.</p>
 
-  @foreach(range(19, 1, -1) as $i)
+  @foreach(range(20, 1, -1) as $i)
 
     <hr>
 
@@ -14,16 +14,22 @@
 
     <div class="row" style="text-align: center">
       <div class="col-sm-4">
-        <img src="http://www.professorfrancken.nl/franckenvrij/webplaatjes/{{ $i }}.1.jpg">
-        <h5>Francken Vrij {{ $i }}.1</h5>
+          <a href="http://www.professorfrancken.nl/franckenvrij/{{ $i }}.1.pdf">
+              <img src="http://www.professorfrancken.nl/franckenvrij/webplaatjes/{{ $i }}.1.jpg">
+              <h5>Francken Vrij {{ $i }}.1</h5>
+          </a>
       </div>
       <div class="col-sm-4">
-        <img src="http://www.professorfrancken.nl/franckenvrij/webplaatjes/{{ $i }}.2.jpg">
-        <h5>Francken Vrij {{ $i }}.2</h5>
+          <a href="http://www.professorfrancken.nl/franckenvrij/{{ $i }}.2.pdf">
+              <img src="http://www.professorfrancken.nl/franckenvrij/webplaatjes/{{ $i }}.2.jpg">
+              <h5>Francken Vrij {{ $i }}.2</h5>
+          </a>
       </div>
       <div class="col-sm-4">
-        <img src="http://www.professorfrancken.nl/franckenvrij/webplaatjes/{{ $i }}.3.jpg">
-        <h5>Francken Vrij {{ $i }}.3</h5>
+          <a href="http://www.professorfrancken.nl/franckenvrij/{{ $i }}.3.pdf">
+              <img src="http://www.professorfrancken.nl/franckenvrij/webplaatjes/{{ $i }}.3.jpg">
+              <h5>Francken Vrij {{ $i }}.3</h5>
+          </a>
       </div>
     </div>
 
