ff99e53ad826c033f9e1feaea9782d1261f4cc1b
Mark Redeman <markredeman@gmail.com>
2016-10-15 12:29:36 +0200

    Add icons to footer using font awesome

--- src/Infrastructure/Http/Views/layout/_head.blade.php
+++ src/Infrastructure/Http/Views/layout/_head.blade.php
@@ -17,5 +17,7 @@
     <link href="/css/home.css" rel="stylesheet">
     <link href="/css/menu.css" rel="stylesheet">
 
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+
     @stack('css')
 </head>
