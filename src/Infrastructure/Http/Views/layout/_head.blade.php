b23ffc3ef1f3420cbed1bd34abd9954bd3cbc45f
Mark Redeman <markredeman@gmail.com>
2016-10-15 12:16:59 +0200

    Add description and title to base layout

--- src/Infrastructure/Http/Views/layout/_head.blade.php
+++ src/Infrastructure/Http/Views/layout/_head.blade.php
@@ -1,9 +1,14 @@
+@section('description')
+‘Professor Francken’ is the study association for Applied Physics, connected to the University of Groningen. It is named after Groningen’s first professor of Applied Physics and is for students and staff of the applied physics departments. It has over 700 members and organizes, among other, field trips in the Netherlands and an annual symposium and a foreign excursion. Various activities, including the introductory activities for first-year students and the Bèta-bedrijvendagen (a career event for science students), are organised in partnership with sister associations. Membership is a must for students with a technical orientation.
+@endsection
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>T.F.V. 'Professor Francken'</title>
+    <title>@yield('title', "T.F.V. 'Professor Francken'")</title>
+    <meta name="description" content="@yield('description')" />
 
     <!-- Bootstrap core CSS -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
