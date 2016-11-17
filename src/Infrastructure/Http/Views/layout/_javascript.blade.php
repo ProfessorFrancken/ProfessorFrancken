3fd65c40b83c3780abc0dbd6a582aa1add15066b
Mark Redeman <markredeman@gmail.com>
2016-11-17 16:59:59 +0100

    Add simple feedback form from UserReport

--- src/Infrastructure/Http/Views/layout/_javascript.blade.php
+++ src/Infrastructure/Http/Views/layout/_javascript.blade.php
@@ -10,4 +10,14 @@
 
 <script src="/js/Menu.js"></script>
 
+<script type="text/javascript">
+window._urq = window._urq || [];
+_urq.push(['initSite', '42efd18f-c4ef-4ad5-a1d8-a430d3f8ef0f']);
+(function() {
+var ur = document.createElement('script'); ur.type = 'text/javascript'; ur.async = true;
+ur.src = ('https:' == document.location.protocol ? 'https://cdn.userreport.com/userreport.js' : 'http://cdn.userreport.com/userreport.js');
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ur, s);
+})();
+</script>
+
 @stack('javascript')
