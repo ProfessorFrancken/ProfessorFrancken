17015f898ca3e94384591d42eee500d7109cd81d
Mark Redeman <markredeman@gmail.com>
2016-11-18 17:08:48 +0100

    Test that the registration request pages render

--- tests/Features/RegistrationRequestFeature.php
+++ tests/Features/RegistrationRequestFeature.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Francken\Features;
 
+use Francken\Application\Members\Registration\RequestStatus;
+use Francken\Application\Members\Registration\RequestStatusRepository;
+use Francken\Domain\Members\Registration\Events\RegistrationRequestSubmitted;
+use Francken\Domain\Members\Registration\RegistrationRequestId;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Francken\Domain\Members\Registration\Events\RegistrationRequestSubmitted;
 
 class RegistrationRequestFeature extends TestCase
 {
@@ -45,5 +48,51 @@ class RegistrationRequestFeature extends TestCase
         $this->seeInDatabase('request_status', [
             'requestee' => 'Mark Redeman'
         ]);
+    }
+
+    /** @test */
+    function listing_all_open_registration_requests()
+    {
+        $requests = $this->app->make(RequestStatusRepository::class);
+        $requests->save(
+            new RequestStatus(
+                new RegistrationRequestId('fffbfb4c-378b-4c76-a0e6-629f3e4e1e9a'),
+                'Mark Redeman',
+                true,
+                true,
+                true,
+                true,
+                \DateTimeImmutable::createFromFormat(
+                    \DateTime::ISO8601,
+                    '2016-11-18T15:52:01+0000'
+                )
+            )
+        );
+
+        $this->visit('/admin/registration-requests')
+            ->see('Mark Redeman');
+    }
+
+    /** @test */
+    function listing_details_of_a_registration_request()
+    {
+        $requests = $this->app->make(RequestStatusRepository::class);
+        $requests->save(
+            new RequestStatus(
+                new RegistrationRequestId('fffbfb4c-378b-4c76-a0e6-629f3e4e1e9a'),
+                'Mark Redeman',
+                true,
+                true,
+                true,
+                true,
+                \DateTimeImmutable::createFromFormat(
+                    \DateTime::ISO8601,
+                    '2016-11-18T15:52:01+0000'
+                )
+            )
+        );
+
+        $this->visit('admin/registration-requests/fffbfb4c-378b-4c76-a0e6-629f3e4e1e9a')
+            ->see('Mark Redeman');
     }
 }
