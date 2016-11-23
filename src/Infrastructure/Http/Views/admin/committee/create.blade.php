33e8ccba334220e932515186271ef7000abf96bf
Mark Redeman <markredeman@gmail.com>
2016-11-23 13:23:19 +0100

    Move error reporting into partial view

--- src/Infrastructure/Http/Views/admin/committee/create.blade.php
+++ src/Infrastructure/Http/Views/admin/committee/create.blade.php
@@ -30,14 +30,6 @@
 
   {!! Form::close() !!}
 
-  @if (count($errors) > 0)
-    <div class="alert alert-danger">
-      <ul>
-        @foreach ($errors->all() as $error)
-          <li>{{ $error }}</li>
-        @endforeach
-      </ul>
-    </div>
-  @endif
+  @include('admin._errors')
 
 @endsection
--- src/Infrastructure/Http/Views/admin/post/create.blade.php
+++ src/Infrastructure/Http/Views/admin/post/create.blade.php
@@ -29,14 +29,5 @@
 
   {!! Form::close() !!}
 
-  @if (count($errors) > 0)
-    <div class="alert alert-danger">
-      <ul>
-        @foreach ($errors->all() as $error)
-          <li>{{ $error }}</li>
-        @endforeach
-      </ul>
-    </div>
-  @endif
-
+  @include('admin._errors')
 @endsection
