c081fe15522a5768e6dd8b8abf43dbdca0ca588a
Mark Redeman <markredeman@gmail.com>
2017-01-22 03:20:40 +0100

    Add some styling for about Francken and the latest Francken Vrij

--- resources/views/homepage/homepage.blade.php
+++ resources/views/homepage/homepage.blade.php
@@ -110,30 +110,41 @@
     <div class="container">
 
         <div class="row">
-            <div class="col">
-                <h2 class="section-header">
-                    About T.F.V. 'Professor Francken'
-                </h2>
+            <div class="col about-francken">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="about-francken__j-c-francken">
+                            <img alt="" src="http://www.professorfrancken.nl/wordpress/media/images/ereleden/profjcfrancken.png" class="rounded-circle" style="width: 200px; height: 200px; overflow: hidden;"/>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <h2 class="section-header">
+                            About T.F.V. 'Professor Francken'
+                        </h2>
 
-                <p>
-                    Orci dapibus ultrices in iaculis nunc sed augue lacus, viverra vitae congue eu, consequat ac felis donec et odio pellentesque diam! Fringilla urna, porttitor rhoncus dolor purus non enim praesent!
-                </p>
+                        <p>
+                            Orci dapibus ultrices in iaculis nunc sed augue lacus, viverra vitae congue eu, consequat ac felis donec et odio pellentesque diam! Fringilla urna, porttitor rhoncus dolor purus non enim praesent!
+                        </p>
+                    </div>
+                </div>
                 <p>
                     Sit amet, aliquam id diam maecenas ultricies mi eget mauris pharetra et ultrices neque ornare aenean euismod elementum nisi, quis? Facilisis sed odio morbi quis commodo odio aenean sed adipiscing.
                 </p>
 
-                <button class="btn btn-primary">Read more</button>
+                <div class="text-right">
+                    <button class="btn btn-primary">Read more</button>
+                </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 offset-md-1">
                 @include("homepage.agenda")
             </div>
 
             <div class="row">
-                <div class="col">
-                    <img alt="" src="https://unsplash.it/300/300"/>
+                <div class="col text-right">
+                    <img alt="" src="https://unsplash.it/500/300"/>
                 </div>
-                <div class="col">
-                    <h3 class="section-header">Francken Vrij</h3>
+                <div class="col-md-5">
+                    <h3 class="section-header">The latest Francken Vrij</h3>
                     <p>
                         Ultricies mi, quis hendrerit dolor magna eget est lorem ipsum dolor sit amet, consectetur adipiscing elit pellentesque habitant morbi tristique senectus et netus et? Malesuada fames ac turpis egestas maecenas.
                     </p>
