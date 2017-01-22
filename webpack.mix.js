496f1f1e3ab75dd614f1b863f214409d982963da
Mark Redeman <markredeman@gmail.com>
2017-01-22 17:11:38 +0100

    Add a note why we're compiling an empty javascript file

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,9 @@ let mix = require('laravel-mix').mix;
   |
 */
 
+// Though we don't currently use mix to compile our javascript, mix will throw
+// an exception if we do not call .js at least once, hence we compile an
+// empty javascript file.
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .version();
