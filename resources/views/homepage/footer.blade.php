235161820d5cc30f2a718220b4b21872960eed3b
Mark Redeman <markredeman@gmail.com>
2017-01-23 16:51:17 +0100

    Begin styling the footer

--- resources/views/homepage/footer.blade.php
+++ resources/views/homepage/footer.blade.php
@@ -1,3 +1,71 @@
-<footer>
+<footer class="mt-4">
+    <div class="row no-gutters">
+        <div class="col-md-4 text-right">
+            <div class="footer__logo align-middle align-items-center">
+                <div class="d-flex justify-content-end align-items-center">
+                    <img
+                        alt=""
+                        src="/images/LOGO_KAAL.png"
+                        height="100px"
+                    />
+                    <h1 class="header__title text-left float-right">
+                        T.F.V.<br/>
+                        'Professor<br/>
+                        Francken'
+                    </h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="d-flex flex-column align-items-stretch h-100 align-content-stretch ">
+                <div class="row no-gutters">
+                    <div class="col">
+                        <p>
+                            &copy; Copyright 2017
+                        </p>
+                    </div>
+                </div>
 
+                <div class="footer__contact h-100">
+                    <div class="row no-gutters align-itmes-stretch h-100">
+                        <div class="col offset-md-2 align-self-center">
+                            <h4>Adress</h4>
+
+                            <address class="footer__body">
+                                Nijenborgh 4<br>
+                                9747AG, Groningen<br>
+                                The Netherlands
+                            </address>
+                        </div>
+                        <div class="col align-self-center">
+                            <h4>Contact</h4>
+
+                            <div class="footer__body">
+                                <i class="fa fa-envelope" aria-hidden="true"></i> <a href="malto: board@professorfrancken.nl">board@professorfrancken.nl</a> <br>
+                                <i class="fa fa-phone" aria-hidden="true"></i> <a href="tel:+310503634978">tel: +31 (0) 50 363 4978</a> <br>
+
+                                <a href="/contact"><u>More contact info</u></a>
+
+                            </div>
+                        </div>
+                        <div class="col align-self-center">
+                            <h4>Social Media</h4>
+
+                            <div class="footer__body">
+                                <i class="fa fa-facebook" aria-hidden="true"></i> Facebook <br>
+                                <i class="fa fa-twitter" aria-hidden="true"></i> Twitter <br>
+                                <a href="https://github.com/ProfessorFrancken/ProfessorFrancken">
+                                    <i class="fa fa-github" aria-hidden="true"></i> Github
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="sponsors">
+
+    </div>
 </footer>
