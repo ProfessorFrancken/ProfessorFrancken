052e34791b4fc8664d5ae5494d884393f5735e43
Mark Redeman <markredeman@gmail.com>
2017-01-29 21:32:37 +0100

    Add Francken Vrij image to homepage

--- resources/views/homepage/about-francken.blade.php
+++ resources/views/homepage/about-francken.blade.php
@@ -1,46 +1,57 @@
 <div class="container">
     <div class="row">
         <div class="col about-francken">
+
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-5">
                     <div class="about-francken__j-c-francken">
                         <img alt="" src="http://www.professorfrancken.nl/wordpress/media/images/ereleden/profjcfrancken.png" class="rounded-circle" style="width: 200px; height: 200px; overflow: hidden;"/>
                     </div>
                 </div>
                 <div class="col">
-                    <h2 class="section-header">
+                    <h2 class="section-header mt-4">
                         About the association
                     </h2>
 
                     <p>
-                        Orci dapibus ultrices in iaculis nunc sed augue lacus, viverra vitae congue eu, consequat ac felis donec et odio pellentesque diam! Fringilla urna, porttitor rhoncus dolor purus non enim praesent!
+                        <em>
+                            Orci dapibus ultrices in iaculis nunc sed augue lacus, viverra vitae congue eu, consequat ac felis donec et odio pellentesque diam! Fringilla urna, porttitor rhoncus dolor purus non enim praesent!
+                        </em>
                     </p>
                 </div>
             </div>
             <p>
-                Sit amet, aliquam id diam maecenas ultricies mi eget mauris pharetra et ultrices neque ornare aenean euismod elementum nisi, quis? Facilisis sed odio morbi quis commodo odio aenean sed adipiscing.
+                Dapibus ultrices in iaculis nunc sed augue lacus, viverra vitae congue eu, consequat ac felis donec. Sit amet, consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus.
             </p>
 
-            <div class="text-right">
+            <div class="text-right mt-4">
                 <button class="btn btn-primary">Read more</button>
             </div>
         </div>
         <div class="col-md-5">
             @include("homepage.agenda")
         </div>
+    </div>
 
-        <div class="row">
-            <div class="col text-right">
-                <img alt="" src="https://unsplash.it/500/300"/>
-            </div>
-            <div class="col-md-5">
-                <h3 class="section-header">The latest Francken Vrij</h3>
-                <p>
-                    Ultricies mi, quis hendrerit dolor magna eget est lorem ipsum dolor sit amet, consectetur adipiscing elit pellentesque habitant morbi tristique senectus et netus et? Malesuada fames ac turpis egestas maecenas.
-                </p>
-                <button class="btn btn-primary">Read me</button>
-                <a class="link-to-all-dark" href="">View all Francken Vrijs</a>
-            </div>
+    <div class="row">
+        <div class="col text-right">
+            <img alt="" src="/images/francken-vrij-homepage.png" class="img-responsive"/>
+        </div>
+        <div class="col-md-5">
+            <h3 class="section-header">The latest Francken Vrij</h3>
+            <p>
+                Lacus, luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo, laoreet sit?
+            </p>
+
+            <ul class="list list-unstyled">
+                <li>Porta nibh venenatis cras sed.</li>
+                <li>Lorem donec massa sapien, faucibus et.</li>
+                <li>Id aliquet risus feugiat in ante metus?</li>
+                <li>Sodales neque sodales ut.</li>
+            </ul>
+
+            <button class="btn btn-primary">Read me</button>
+            <a class="link-to-all-dark" href="">View all Francken Vrijs</a>
         </div>
     </div>
 </div>
