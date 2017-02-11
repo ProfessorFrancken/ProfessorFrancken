b954c92e6cec9930ea69d3fee81e2ba7f7378844
Mark Redeman <markredeman@gmail.com>
2017-02-11 14:56:16 +0100

    Make logo clickable

--- resources/views/homepage/_logo.blade.php
+++ resources/views/homepage/_logo.blade.php
@@ -1,15 +1,16 @@
 <div class="header__logo align-middle align-items-center">
     <div class="d-flex justify-content-end align-items-center">
-
-        <img
-            alt=""
-            src="/images/LOGO_KAAL.png"
-            height="100px"
-        />
-        <h1 class="header__title text-left float-right">
-            T.F.V.<br/>
-            'Professor<br/>
-            Francken'
-        </h1>
+        <a class="header__title-link" href="/">
+            <img
+                alt=""
+                src="/images/LOGO_KAAL.png"
+                height="100px"
+            />
+            <h1 class="header__title text-left float-right">
+                T.F.V.<br/>
+                'Professor<br/>
+                Francken'
+            </h1>
+        </a>
     </div>
 </div>
