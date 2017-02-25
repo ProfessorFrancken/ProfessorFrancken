8aa13326c28de29c966b4ca924e8f2d4000cbcd5
Mark Redeman <markredeman@gmail.com>
2017-02-25 23:31:49 +0100

    Move code of hamburger menu into a partial

--- resources/views/homepage/_logo.blade.php
+++ resources/views/homepage/_logo.blade.php
@@ -1,15 +1,19 @@
-@section('hamburger-menu')
-    {{-- This section adds the Hamburger menu --}}
-    <div class="navigation__mobile-menu">
-        <button id="navbar-toggler" class="hamburger-menu">
-            <span class="hamburger-menu__line"></span>
-            <span class="hamburger-menu__line"></span>
-            <span class="hamburger-menu__line"></span>
-        </button>
-    </div>
-@endsection
+<div class="skew-md--top-right header__logo d-flex justify-content-between justify-content-md-end">
+    <a class="header__title-link align-items-center align-middle d-inline-flex" href="/">
+        <img
+            alt="Logo of T.F.V. 'Professor Francken'"
+            src="/images/LOGO_KAAL.png"
+            class="img-fluid"
+        />
+        <h1 class="header__title text-left">
+            T.F.V.<br class="hidden-md-down"/>
+            'Professor<br class="hidden-md-down"/>
+            Francken'
+        </h1>
+    </a>
 
-@section('menu-items')
+    @include('homepage.navigation._hamburger')
+
     {{--
 
         This section contains all of the menu items
@@ -27,18 +31,4 @@
             ])
         @endforeach
     </ul>
-@endsection
-
-<div class="skew-md--top-right header__logo d-flex justify-content-between justify-content-md-end">
-    <a class="header__title-link align-items-center align-middle d-inline-flex" href="/">
-        <img alt="Logo of T.F.V. 'Professor Francken'" src="/images/LOGO_KAAL.png" class="img-fluid" />
-        <h1 class="header__title text-left">
-            T.F.V.<br class="hidden-md-down"/>
-            'Professor<br class="hidden-md-down"/>
-            Francken'
-        </h1>
-    </a>
-
-    @yield('hamburger-menu')
-    @yield('menu-items')
 </div>
