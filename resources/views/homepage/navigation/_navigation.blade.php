c36563876890f0a4037d11424f7e617d67a1f7aa
Mark Redeman <markredeman@gmail.com>
2017-02-27 23:28:04 +0100

    Remove unnecessary html in navigation

--- resources/views/homepage/navigation/_navigation.blade.php
+++ resources/views/homepage/navigation/_navigation.blade.php
@@ -1,29 +1,19 @@
-<div class="header__navigation h-100">
-    <div class="no-gutters h-100">
-        <div class="align-items-center h-100">
-            <div class="d-flex align-items-center h-100">
-                <div class="navigation">
-                    <nav class="navigation__menu nav justify-content-around">
-                        @foreach ($items as $item)
-                            <a class="navigation__menu-item nav-link active text-nowrap {{ $item['class'] or '' }}" href="{{ $item['url'] }}">
-                                @if ($item['icon'] != '')
-                                    <i class="fa fa-{{ $item['icon'] }} mr-2" aria-hidden="true"></i>
-                                @endif
-                                {{ $item['title'] }}
-                            </a>
-                        @endforeach
-                    </nav>
+<div class="navigation__menu nav">
+    @foreach ($items as $item)
+        <div class="navigation__hoverable">
+            <a class="navigation__menu-item nav-link active text-nowrap {{ $item['class'] or '' }}" href="{{ $item['url'] }}">
+                @if ($item['icon'] != '')
+                    <i class="fa fa-{{ $item['icon'] }} mr-2" aria-hidden="true"></i>
+                @endif
+                {{ $item['title'] }}
+            </a>
 
-                    <nav class="navigation__sub-menu nav justify-content-end">
-                        @foreach ($items as $item)
-                            <?php $active = '/' . Request::segment(1) == $item['url'] ? 'navigation__sub-menu-item--active' : ''; ?>
-                                @foreach ($item['subItems'] as $subItem)
-                                    <a class="navigation__sub-menu-item nav-link {{ $active }} text-nowrap" href="{{ $subItem['url'] }}">{{ $subItem['title'] }}</a>
-                                @endforeach
-                        @endforeach
-                    </nav>
-                </div>
-            </div>
+            <nav class="navigation__sub-menu nav justify-content-end" >
+                <?php $active = '/' . Request::segment(1) == $item['url'] ? 'navigation__sub-menu-item--active' : ''; ?>
+                @foreach ($item['subItems'] as $subItem)
+                    <a class="navigation__sub-menu-item nav-link {{ $active }} text-nowrap" href="{{ $subItem['url'] }}">{{ $subItem['title'] }}</a>
+                @endforeach
+            </nav>
         </div>
-    </div>
+    @endforeach
 </div>
