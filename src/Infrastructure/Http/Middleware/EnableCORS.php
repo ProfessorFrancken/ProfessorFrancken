1813d13e7da82af2eb85992f6d0979f0dbd8c309
Mark Redeman <markredeman@gmail.com>
2017-03-28 19:21:57 +0200

    Add EnableCORS middleware

---
+++ src/Infrastructure/Http/Middleware/EnableCORS.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Francken\Infrastructure\Http\Middleware;
+
+use Closure;
+
+final class EnableCORS
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        if ($request->segment(2) === "pluimpje") {
+            return $response->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        }
+
+        return $response;
+    }
+}
