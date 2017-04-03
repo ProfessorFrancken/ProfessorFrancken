6228f1ee9ba214cc2d4820375e4a463d5385f939
Mark Redeman <markredeman@gmail.com>
2017-04-03 12:00:36 +0200

    Add a section for the aside element of a two column page
    In order to make it easier to add custom aside components for committees,
    research groups and m ore pages, the css for the agenda component has been moved
    to aside.scss. We will refactor this later on so that this is no longer specific
    to agenda items.

--- resources/views/homepage/two-column-layout.blade.php
+++ resources/views/homepage/two-column-layout.blade.php
@@ -6,9 +6,11 @@
             <div class="col">
                 @yield('content')
             </div>
-            <div class="col-md-5">
+            <div class="col-lg-5">
                 <div class="agenda-wrapper">
-                    @include("homepage._agenda")
+                    @section('aside')
+                        @include("homepage._agenda")
+                    @show
                 </div>
             </div>
         </div>
@@ -17,9 +19,5 @@
 
 @section('header-image')
     @component('homepage.header._header_image')
-    @slot('headerImageClass')
-        header__registration-cta--small
-    @endslot
-
     @endcomponent
 @endsection
