64ca1618d87fd518e76746dba8bee612ae22aba2
Mark Redeman <markredeman@gmail.com>
2017-04-03 12:22:00 +0200

    Show list of research groups in the aside component

--- resources/views/pages/study/research-groups.blade.php
+++ resources/views/pages/study/research-groups.blade.php
@@ -147,3 +147,37 @@ $groups = [
     </div>
     @endcomponent
 @endsection
+
+@section('aside')
+<div class="agenda">
+    <h3 class="section-header agenda-header">
+        Research groups
+    </h3>
+    <ul class="agenda-list list-unstyled">
+        @foreach ($groups as $group)
+
+            <li class="agenda-item" style="margin-bottom: .5em; padding-bottom: .5em;">
+                <a
+                    href="/study/research-groups"
+                    class="aside-link"
+                >
+                    <div class="media align-items-center">
+                        <div class="media-body">
+                            <h5 class="agenda-item__header">
+                                {{ $group['title'] }}
+                            </h5>
+                        </div>
+                        <img
+                            class="rounded d-flex ml-3"
+                            src="https://www.rug.nl{{ $group['photo'] or '' }}"
+                            alt="{{ $group['title'] }}'s logo"
+                            style="width: 75px; height: 75px; object-fit: cover; border-radius: 50%;"
+                        >
+                    </div>
+                </a>
+
+            </li>
+        @endforeach
+    </ul>
+</div>
+@endsection
