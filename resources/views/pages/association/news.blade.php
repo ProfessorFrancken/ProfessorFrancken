537253746de772f705a854dcbfc5235f8825ac75
Mark Redeman <markredeman@gmail.com>
2017-04-15 23:54:41 +0200

    Add better styling of news preview items

--- resources/views/pages/association/news.blade.php
+++ resources/views/pages/association/news.blade.php
@@ -1,46 +1,42 @@
 @extends('pages.association')
 @inject('faker', "Faker\Generator")
 
-@section('sub-menu')
-    @include('layout._subnavigation', [
-        'list' => [
-            ['url' => "/post", 'title' => 'All'],
-            ['url' => "/news", 'title' => 'News'],
-            ['url' => "/blog", 'title' => 'Blog'],
-        ]
-    ])
-@endsection
-
 @section('main-content')
-    <div class="container">
-        <h2 class="section-header">
+    <div class="container my-5">
+        <h2 class="section-header section-header--centered">
             The latest news
         </h2>
 
         <div class="ribbon__items row align-items-stretch">
             @foreach (range(1, 12) as $r)
-                @component('homepage._news-item')
-                    @slot('title')
-                    {{ $faker->sentence() }}
-                    @endslot
+            <div class="col-md-4 news-item">
+                <article class="h-100 preview-item d-flex flex-column justify-content-between">
+                    <div style="">
+                        <div class="news-item__header">
+                            <span class="news-item__date badge preview-item__date">
+                                {{ $faker->date('d M Y') }}
+                            </span>
+                            <span class="news-item__written-by">
+                                Posted by
+                                <span class="news-item__author">
+                                    {{ $faker->name() }}
+                                </span>
+                            </span>
+                        </div>
+                        <h4 class="news-item__title preview-item__title">
+                            {{ $faker->sentence() }}
+                        </h4>
+                        <p class="news-item__body">
+                            {{ $faker->paragraph() }}
+                        </p>
+                    </div>
 
-                    @slot('date')
-                        {{ $faker->date('d M Y') }}
-                    @endslot
-
-
-                    @slot('author')
-                        {{ $faker->name() }}
-                    @endslot
-
-                    {{ $faker->paragraph() }}
-                @endcomponent
+                    <div>
+                        <button class="btn btn-inverse">Read more</button>
+                    </div>
+                </article>
+            </div>
             @endforeach
-        </div>
-        <div class="text-md-right">
-            <a class="link-to-all arrow" href="/association/news">
-                View all news
-            </a>
         </div>
     </div>
 @endsection
