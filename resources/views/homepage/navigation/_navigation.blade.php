4f435aba4e2b145ababdf3591a3b0cef0a3f151d
Mark Redeman <markredeman@gmail.com>
2017-05-01 22:58:22 +0200

    Cleanup of html of the navigation and logo
    This should make it easier to add a "Back to [page]" button below the logo.

--- resources/views/homepage/navigation/_navigation.blade.php
+++ resources/views/homepage/navigation/_navigation.blade.php
@@ -1,7 +1,16 @@
 <div class="navigation__menu nav">
     @foreach ($items as $item)
         <div class="navigation__hoverable">
-            <a class="navigation__menu-item nav-link active text-nowrap {{ $item['class'] or '' }}" href="{{ $item['url'] }}">
+            <?php $active = '/' . Request::segment(1) == $item['url'] ? 'navigation__menu-item--active' : ''; ?>
+            <a
+                class="
+                       navigation__menu-item
+                       nav-link
+                       text-nowrap
+                       {{ $active }}
+                       {{ $item['class'] or '' }}"
+                href="{{ $item['url'] }}"
+            >
                 @if ($item['icon'] != '')
                     <img src="/icons/filled/{{ $item['icon'] }}.svg" alt="" style="height: 20px" class="mr-2">
                 @endif
@@ -11,7 +20,12 @@
             <nav class="navigation__sub-menu nav justify-content-end" >
                 <?php $active = '/' . Request::segment(1) == $item['url'] ? 'navigation__sub-menu-item--active' : ''; ?>
                 @foreach ($item['subItems'] as $subItem)
-                    <a class="navigation__sub-menu-item nav-link {{ $active }} text-nowrap" href="{{ $subItem['url'] }}">{{ $subItem['title'] }}</a>
+                    <a
+                        class="navigation__sub-menu-item nav-link {{ $active }} text-nowrap"
+                        href="{{ $subItem['url'] }}"
+                    >
+                        {{ $subItem['title'] }}
+                    </a>
                 @endforeach
             </nav>
         </div>
