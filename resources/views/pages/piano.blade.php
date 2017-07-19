46505596b8b70b1a90f7ecbc644f8cf35d79db5b
Mark Redeman <markredeman@gmail.com>
2017-07-19 17:20:01 +0200

    Add a page about the piano fundraiser.

---
+++ resources/views/pages/piano.blade.php
@@ -0,0 +1,66 @@
+@extends('homepage.one-column-layout')
+@section('title', '')
+@section('description', '')
+
+@section('header-image-url', 'https://images3.persgroep.net/rcs/kFiWtBVNnKswrVgF7zYT6pnqrlI/diocontent/100809956/_fill/1164/900/?appId=21791a8992982cd8da851550a453bd7f&quality=0.9')
+
+@section('content')
+    <h1 class="section-header section-header--centered">The Francken Piano</h1>
+
+    <p class="lead">
+        On the 12th of June 2017, the Fysisch-Mathematische Faculteitsvereniging gifted our beloved association with a beatiful Julius Pfaffe Piano. Ever since this beloved day, the piano has experienced a great many adventures.
+    </p>
+
+    <div class="text-center">
+        <img alt="" src="https://static.roland.com/assets/images/products/gallery/v-piano_grand_angle_open_full_gal.jpg" class="img-fluid rounded" style="height: 400px; width: 100%; object-fit: cover; object-position: 50% 50%;"/>
+    </div>
+
+    <h2 class="section-header section-header--centered mt-5">
+        Be the hero we need, not the one we deserve
+    </h2>
+
+    <p class="text-center lead">
+        Our Julius Pfaffe Piano is old and needs help recovering from its adventures.
+        Since we do not want the piano's sound to resesemble the sound of a crying baby on a cool summer night we have begun a <strong>fundraising</strong>.
+        For only €60,- we will be able to give the piano new life.
+        This money will be used to tune the piano's strings so that instead its sounds will be as if an angel is peeing into your ears.
+    </p>
+
+    <div class="text-center">
+        <img alt="" src="http://kestermusic.com/wp-content/uploads/sites/13/CS57145.jpg" class="img-fluid rounded" style="height: 400px; width: 100%; object-fit: cover; object-position: 50% 50%;"/>
+    </div>
+
+
+    <h3 class="section-header section-header--centered mt-5">
+        Will you be our hero?
+    </h3>
+
+    <p class="text-center lead">
+        Arjen Kramer will be hapy to accept any form of donations whether it be in pay in kind, or in cash.
+    </p>
+
+    <table class="table mt-4">
+        <thead class="thead-default">
+            <tr>
+                <th>Donor</th>
+                <th>Form</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Mark Redeman</td>
+                <td>Creating this beatifull webpage</td>
+            </tr>
+            <tr>
+                <td>Jasper Pluijmers</td>
+                <td>€10,-</td>
+            </tr>
+            <tr>
+                <td>Arjen Kramer</td>
+                <td>€10,-</td>
+            </tr>
+        </tbody>
+    </table>
+
+
+@endsection
