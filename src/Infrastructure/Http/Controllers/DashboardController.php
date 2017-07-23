4234e9f61d441cddac168610582b67c26fc545b7
Mark Redeman <markredeman@gmail.com>
2017-07-23 13:11:55 +0200

    Correct redirect of DashboardController

--- src/Infrastructure/Http/Controllers/DashboardController.php
+++ src/Infrastructure/Http/Controllers/DashboardController.php
@@ -10,7 +10,7 @@ class DashboardController extends Controller
 {
     public function redirectToDashboard()
     {
-        return redirect('/admin/dashboard');
+        return redirect('/admin/overview');
     }
 
     public function overview()
