9c557843432290b6814d7fa0d609a3e087e10503
Mark Redeman <markredeman@gmail.com>
2017-07-24 12:22:48 +0200

    Add a subtle box-shadow around the billing details selection

--- resources/views/registration/_billing-details.blade.php
+++ resources/views/registration/_billing-details.blade.php
@@ -2,8 +2,8 @@
  .grow {
      transition: all .2s ease-in-out;
      transform: scale(1.1);
-     border: thin solid #333;
      margin: 2em 0;
+     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
  }
 
  .custom-control {
