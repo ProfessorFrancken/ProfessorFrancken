8db411cc8d7d0509eb09deef194407133b9bfcb1
Mark Redeman <markredeman@gmail.com>
2017-07-27 20:46:01 +0200

    Improve markup of university council

--- resources/views/pages/study/representation/university-council.blade.php
+++ resources/views/pages/study/representation/university-council.blade.php
@@ -2,47 +2,54 @@
 @section('title', "University Council - T.F.V. 'Professor Francken'")
 
 @section('content')
-<div class="section-header d-inline-block mt-4 h1">University Council</div>
-<div class="text-justify row mt-2">
-<div class="col-md-7">
-The University Council is the highest co-determination body of our university. Each month, twelve students and twelve staff members meet with the Board of the University to discuss university-wide policies. The main task of the University Council is to check the policy-making by the Board of the University. During the monthly University Council meeting, the council discusses a wide range of policies: from the general Teaching- and Exam Regulations, to working conditions, the renovation of university buildings or the annual university budget. On some of these topics, the University Council has the right of consent: this means that the council must vote on specific policy documents. On other documents, the council had the right to provide an advice to the Board of the University, which the board must take seriously. And some policy documents are only to be discussed, or sent to the council for information purposes.  
-<br><br>
-The student factions of the University Council also have the right to propose their own policy initiatives. They do so by writing a memo to the Board of the University in which they propose a new policy. You can find proposals that they’ve handed in over the past years on their websites.  
-<br><br>
-At the moment, there are three student parties: Lijst Calimero, SOG and Lijst Sterk. How the twelve student seats are divided among the parties is decided each year, during the university elections. The staff members are elected every two years.
-<br>
-</div>
-<div class="col-md-5">
-<div class="row">
-	<div class="col h2">
-		Contacts
-	</div>
-	<div class="col ml-0">
-		<div class="media ml-0 mt-2 rounded" style="background:#0fb13a">
-			<img src="https://www.lijstcalimero.nl/wp-content/uploads/2016/09/Fractie_Hanne.jpg" height="200" class="rounded"/>
-			<div class="media-body ml-4" style="color:white">
-				<img src="http://betastuf.nl/wp-content/uploads/2017/01/Logo-wit.png" height="80" align="top" class="ml-2 mt-2"/>
-				<br>
-				<br><b>Hanne Oelen</b>
-				<br>hanneoelen@lijstcalimero.nl
-				<br>06 488 06 112
-			</div>
-		</div>
-	</div>
-	<div class="col ml-0">
-		<div class="media ml-0 mt-2 rounded" style="background:#d69c29">
-			<img src="http://studentenorganisatie.nl/wp-content/uploads/2014/09/Cornesmall.jpg" height="200" width="150" class="rounded"/>
-			<div class="media-body ml-4" style="color:white">
-				<img src="http://studentenorganisatie.nl/wp-content/uploads/2015/01/logoSOG_klein.png" style="filter:brightness(0) invert(1)" height="80" align="top" class="ml-2 mt-2"/>
-				<br>
-				<br><b>Cornelis Zandt</b>
-				<br>cornelis@studentenorganisatie.nl
-				<br>06 233 38 298
-			</div>
-		</div>
-	</div>
-</div>
-</div>
-</div>
+    <h2 class="section-header d-inline-block mt-4 h1">
+        University Council
+    </h2>
+    <div class="row">
+        <div class="col-lg-7">
+            <p class="text-justify">
+                The University Council is the highest co-determination body of our university. Each month, twelve students and twelve staff members meet with the Board of the University to discuss university-wide policies. The main task of the University Council is to check the policy-making by the Board of the University. During the monthly University Council meeting, the council discusses a wide range of policies: from the general Teaching- and Exam Regulations, to working conditions, the renovation of university buildings or the annual university budget. On some of these topics, the University Council has the right of consent: this means that the council must vote on specific policy documents. On other documents, the council had the right to provide an advice to the Board of the University, which the board must take seriously. And some policy documents are only to be discussed, or sent to the council for information purposes.
+            </p>
+            <p class="text-justify">
+                The student factions of the University Council also have the right to propose their own policy initiatives. They do so by writing a memo to the Board of the University in which they propose a new policy. You can find proposals that they’ve handed in over the past years on their websites.
+            </p>
+            <p class="text-justify">
+                At the moment, there are three student parties: Lijst Calimero, SOG and Lijst Sterk. How the twelve student seats are divided among the parties is decided each year, during the university elections. The staff members are elected every two years.
+            </p>
+        </div>
+        <div class="col-lg-5">
+            <h2>
+		            Contacts
+            </h2>
 
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-12">
+                    <div class="media ml-0 mt-2 rounded" style="background:#0fb13a">
+			                  <img src="https://www.lijstcalimero.nl/wp-content/uploads/2016/09/Fractie_Hanne.jpg" height="200" class="rounded"/>
+			                  <div class="media-body ml-4" style="color:white">
+				                    <img src="http://betastuf.nl/wp-content/uploads/2017/01/Logo-wit.png" height="80" align="top" class="ml-2 mt-2"/>
+				                    <br>
+				                    <br><b>Hanne Oelen</b>
+				                    <br>hanneoelen@lijstcalimero.nl
+				                    <br>06 488 06 112
+			                  </div>
+		                </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-12">
+                    <div class="media ml-0 mt-2 rounded" style="background:#d69c29">
+			                  <img src="http://studentenorganisatie.nl/wp-content/uploads/2014/09/Cornesmall.jpg" height="200" width="150" class="rounded"/>
+			                  <div class="media-body ml-4" style="color:white">
+				                    <img src="http://studentenorganisatie.nl/wp-content/uploads/2015/01/logoSOG_klein.png" style="filter:brightness(0) invert(1)" height="80" align="top" class="ml-2 mt-2"/>
+				                    <br>
+				                    <br><b>Cornelis Zandt</b>
+				                    <br>cornelis@studentenorganisatie.nl
+				                    <br>06 233 38 298
+			                  </div>
+		                </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
 @endsection
