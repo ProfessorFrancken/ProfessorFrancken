98697f16ee73b450586d591d7ea8164721f43c69
Mark Redeman <markredeman@gmail.com>
2017-07-31 15:32:18 +0200

    Remove old layout files

--- resources/views/layout/_header.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Header -->
-<!-- background-image: url('http://www.professorfrancken.nl/wordpress/wp-content/uploads/2013/11/das_header-1080x400.png') -->
-    <header class="front-header">
-    <div class="front-header__internal">
-        <h1 class="front-header__header">T.F.V. 'Professor Francken'</h1>
-    </div>
-</header>
--- resources/views/layout/_subnavigation.blade.php
+++
@@ -1,9 +0,0 @@
-<ul class="nav navbar-nav">
-    @foreach ($list as $item)
-        <li class="sub-menu-item">
-            <a href="{{ $item['url'] }}">
-                {{ $item['title'] }}
-            </a>
-        </li>
-    @endforeach
-</ul>
