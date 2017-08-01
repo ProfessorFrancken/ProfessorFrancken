541b024edadc161db246e96ab4ffc60d84a93503
Mark Redeman <markredeman@gmail.com>
2017-08-01 23:42:44 +0200

    Order events shown in dashboard by date and only show a few

--- src/Infrastructure/Http/Controllers/DashboardController.php
+++ src/Infrastructure/Http/Controllers/DashboardController.php
@@ -15,7 +15,7 @@ class DashboardController extends Controller
 
     public function overview()
     {
-        $events = DB::table('event_store')->get();
+        $events = DB::table('event_store')->orderBy('recorded_on', 'desc')->take(25)->get();
 
         return view('admin.overview', [
             'events' => $events
