890e23db8db824e91e56a0ea7fd32ee0d8d06b53
Mark Redeman <markredeman@gmail.com>
2017-08-01 23:44:40 +0200

    Use a two column layout in the admin pages

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -1,108 +1,115 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('admin.base-layout')
 
-        @include('layout._seo')
+<?php
 
-        <link rel="stylesheet" href="{{ mix('/dist/css/app.css') }}">
+$menu = [
+    [
+        "name" => "Association",
+        "url" => "association",
+        "items" => [
+            [
+                "name" => "Blogs",
+                "url" => "blogs",
+                "works" => false,
+            ],
+            [
+                "name" => "Activities",
+                "url" => "activities",
+                "works" => false,
+            ],
+            [
+                "name" => "Open registrations",
+                "url" => "registration-requests",
+                "works" => true,
+            ],
+            [
+                "name" => "Members",
+                "url" => "members",
+                "works" => false,
+            ],
+            [
+                "name" => "Committees",
+                "url" => "committees",
+                "works" => false,
+            ],
+            [
+                "name" => "Francken Vrij",
+                "url" => "francken-vrij",
+                "works" => true,
+            ],
+        ]
+    ],
+    [
+        "name" => "Study",
+        "url" => "study",
+        "items" => [
+            [
+                "name" => "Research Groups",
+                "url" => "research-groups",
+                "works" => false,
+            ],
+            [
+                "name" => "Books",
+                "url" => "books",
+                "works" => false,
+            ],
+        ]
+    ],
+    [
+        "name" => "Career",
+        "url" => "career",
+        "items" => [
+            [
+                "name" => "Companies",
+                "url" => "companies",
+                "works" => false,
+            ],
+            [
+                "name" => "Events",
+                "url" => "events",
+                "works" => false,
+            ],
+            [
+                "name" => "Jop openings",
+                "url" => "jop-openings",
+                "works" => false,
+            ],
+        ]
+    ],
+];
+?>
 
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css" integrity="sha384-wITovz90syo1dJWVh32uuETPVEtGigN07tkttEqPv+uR2SE/mbQcG7ATL28aI9H0" crossorigin="anonymous">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.js" integrity="sha384-/y1Nn9+QQAipbNQWU65krzJralCnuOasHncUFXGkdwntGeSvQicrYkiUBwsgUqc1" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/contrib/auto-render.min.js" integrity="sha384-dq1/gEHSxPZQ7DdrM82ID4YVol9BYyU7GbWlIwnwyPzotpoc57wDw/guX8EaYGPx" crossorigin="anonymous"></script>
+@section('main-content')
+    <div class="row">
+        <div class="col-12 col-lg-2 col-md-3 bd-sidebar">
+            <nav class="bd-links" id="docsNavbarContent">
+                @foreach ($menu as $item)
 
-        @include('layout._favicon')
-    </head>
-    <body>
-        <nav class="navbar navbar-toggleable-md navbar-inverse bg-primary">
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <a class="navbar-brand" href="/admin">Francken admin</a>
+                <div class="bd-toc-item active">
+                    <span class="bd-toc-link">
+                        {{ $item['name'] }}
+                    </span>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Study
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="/admin/study/research-groups">Research Groups</a>
-                            <a class="dropdown-item" href="/admin/study/books">Books</a>
-                        </div>
-                    </li>
+                    <ul class="nav bd-sidenav">
+                        @foreach ($item['items'] as $subItem)
+                        <?php $active = Request::segment(3) == $subItem['url'] ? 'active' : ''; ?>
+                        <li class="{{ $active }}">
+                            <a  href="/admin/{{ $item['url'] }}/{{ $subItem['url'] }}">
+                                @if (! $subItem['works'])
+                                    <i class="fa fa-times" aria-hidden="true"></i>
+                                @endif
 
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Career
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="/admin/career/companies">Companies</a>
-                            <a class="dropdown-item" href="/admin/career/events">Events</a>
-                            <a class="dropdown-item" href="/admin/career/job-openings">Job openings</a>
-                        </div>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Association
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="/admin/association/blog">Blogs</a>
-                            <a class="dropdown-item" href="/admin/association/activities">Activities</a>
-                            <a class="dropdown-item" href="/admin/association/members">Members</a>
-                            <a class="dropdown-item" href="/admin/association/committees">Committees</a>
-                            <a class="dropdown-item" href="/admin/association/registration-requests">Registrations</a>
-                            <a class="dropdown-item" href="/admin/association/francken-vrij">Francken Vrij</a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-
-        <main>
-            <div class="container-fluid">
-                @yield('content')
-            </div>
-        </main>
-
-
-        @include('layout._login-modal')
-
-        <script type="text/javascript">
-         // UserReport (gather feedback)
-         window._urq = window._urq || [];
-         _urq.push(['initSite', '42efd18f-c4ef-4ad5-a1d8-a430d3f8ef0f']);
-         (function() {
-             var ur = document.createElement('script'); ur.type = 'text/javascript'; ur.async = true;
-             ur.src = ('https:' == document.location.protocol ? 'https://cdn.userreport.com/userreport.js' : 'http://cdn.userreport.com/userreport.js');
-             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ur, s);
-         })();
-
-         // Render  any latex currently loaded in our dom
-         document.addEventListener("DOMContentLoaded", function() {
-             renderMathInElement(document.body);
-         });
-
-         @if (config('francken.general.google-analytics'))
-         // Google Analytics
-         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                                  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-         ga('create', '{{ config('francken.general.google-analytics') }}', 'auto');
-         ga('send', 'pageview');
-         @endif
-
-        </script>
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"> </script>
-        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-    </body>
-</html>
+                                {{ $subItem['name'] }}
+                            </a>
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endforeach
+            </nav>
+        </div>
+        <div class="col-12 col-lg-7 col-md-9 bd-content">
+            @yield('content')
+        </div>
+    </div>
+@endsection
