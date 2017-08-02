9e908d8672ab014d6b43ae0a3cb3f198acd3104b
Kangeroebig <jasper.pluijmers@gmail.com>
2017-08-02 15:51:50 +0200

    Update piano.blade.php
    +steven+delan

--- resources/views/pages/piano.blade.php
+++ resources/views/pages/piano.blade.php
@@ -59,6 +59,14 @@
                 <td>Arjen Kramer</td>
                 <td>€10,-</td>
             </tr>
+            <tr>
+                <td>Steven Groen</td>
+                <td>€10,-</td>
+            </tr>
+            <tr>
+                <td>Delan Caglar (met gekke kringeltjes onderletters)</td>
+                <td>€10,-</td>
+            </tr>
         </tbody>
     </table>
 
