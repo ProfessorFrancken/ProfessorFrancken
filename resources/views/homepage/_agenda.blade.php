961ba4c2b39ee4189b05e21d56afc477f93c4d9a
Mark Redeman <markredeman@gmail.com>
2017-08-31 13:23:45 +0200

    Update agenda and add download link to ical

--- resources/views/homepage/_agenda.blade.php
+++ resources/views/homepage/_agenda.blade.php
@@ -2,36 +2,30 @@
 
 $activities = [
     [
-        'date' => new \DateTimeImmutable('07-08-2017'),
-        'title' => 'Watch Game of Thrones at Francken',
-        'short-description' => '',
-    ],
-    [
-        'date' => new \DateTimeImmutable('14-08-2017'),
-        'title' => 'Watch Game of Thrones at Francken',
-        'short-description' => '',
-    ],
-    [
-        'date' => new \DateTimeImmutable('14-08-2017'),
-        'title' => 'KEI-week 2017',
-        'short-description' => '',
-    ],
-    [
-        'date' => new \DateTimeImmutable('21-08-2017'),
-        'title' => 'Watch Game of Thrones at Francken',
-        'short-description' => '',
-    ],
-    [
-        'date' => new \DateTimeImmutable('28-08-2017'),
-        'title' => 'Watch Game of Thrones at Francken',
-        'short-description' => '',
-    ],
-    [
         'date' => new \DateTimeImmutable('01-09-2017'),
         'title' => 'Pienterkamp',
         'short-description' => 'Introduction camp for (Applied) Mathematics and (Applied) Physics.',
     ],
-
+    [
+        'date' => new \DateTimeImmutable('04-09-2017'),
+        'title' => "Free tosties",
+        'short-description' => "Get your free tosti during the first week in the Franckenboom",
+    ],
+    [
+        'date' => new \DateTimeImmutable('05-09-2017'),
+        'title' => 'Cocktailborrel',
+        'short-description' => 'Drink some cocktails after your lectures at Francken',
+    ],
+    [
+        'date' => new \DateTimeImmutable('07-09-2017'),
+        'title' => '1st Years Applied Physics Burger Night',
+        'short-description' => 'Eat some burgers at the Papa Joe steak restaurant',
+    ],
+    [
+        'date' => new \DateTimeImmutable('11-09-2017'),
+        'title' => 'Werewolves of Franckendam',
+        'short-description' => '',
+    ],
 ];
 
 ?>
@@ -55,5 +49,21 @@ $activities = [
                 </div>
             </li>
         @endforeach
+        <li class="agenda-item d-flex d-flex align-items-center">
+            <div class="agenda-item__date align-self-start">
+                <h5 class="agenda-item__date-day">
+                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                </h5>
+            </div>
+
+            <div class="agenda-item__body">
+                <a href="https://calendar.google.com/calendar/ical/g8f50ild2kdf49bgathcdhvcqc%40group.calendar.google.com/public/basic.ics">
+                    <h5 class="agenda-item__header">Download our ical</h5>
+                    <p class="agenda-item__description">
+                        Upload our agenda to your own by downloading our ical
+                    </p>
+                </a>
+            </div>
+        </li>
     </ul>
 </div>
