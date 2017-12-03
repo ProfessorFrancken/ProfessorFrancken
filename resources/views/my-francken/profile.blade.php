152594a86362dbea666ba4ef5e59b4aa94a8722e
Mark Redeman <markredeman@gmail.com>
2017-12-03 23:20:31 +0100

    Show francken profile

--- resources/views/my-francken/profile.blade.php
+++ resources/views/my-francken/profile.blade.php
@@ -3,6 +3,13 @@
 @section('content')
     <h2 class="section-header">
         <i class="fa fa-user text-primary text-center" aria-hidden="true"></i>
-        Profile
+        Hi, {{ $profile->voornaam }}
     </h2>
+
+    @include('my-francken.profile._personal')
+    @include('my-francken.profile._bank')
+    @include('my-francken.profile._study')
+    @include('my-francken.profile._career')
+    @include('my-francken.profile._settings')
+
 @endsection
---
+++ resources/views/my-francken/profile/_personal.blade.php
@@ -0,0 +1,27 @@
+<div class="card my-3">
+    <div class="card-header">
+        <h3>Personal Information</h3>
+    </div>
+
+    <div class="card-body">
+        <ul>
+            <li>Picture</li>
+            <li>Firstname, lastname: {{ $profile->voornaam }} {{ $profile->achternaam }}</li>
+            <li>Mother Tongue: {{ $profile->land }}</li>
+            <li>Birthdate: {{ $profile->geboortedatum }}</li>
+            <li>Gender: {{ $profile->geslacht }}</li>
+        </ul>
+    </div>
+</div>
+
+<div class="card my-3">
+    <div class="card-header">
+        <h4>Contact details</h4>
+    </div>
+    <div class="card-body">
+        <ul>
+            <li>Email: {{ $profile->emailadres }}</li>
+            <li>Address {{ $profile->plaats }}, {{ $profile->postcode }}, {{ $profile->adres }}</li>
+        </ul>
+    </div>
+</div>
