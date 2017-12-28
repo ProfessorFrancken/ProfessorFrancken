1204b60e8cdccb5dac5f01fccc649330efa21f00
SuStreepjeElle <30695375+SuStreepjeElle@users.noreply.github.com>
2017-12-28 14:24:56 +0100

    Update adtcie.blade.php
    Forgot to spell adtie with a capital A. Description for the Adtie.

--- resources/views/pages/association/committees/adtcie.blade.php
+++ resources/views/pages/association/committees/adtcie.blade.php
@@ -2,6 +2,6 @@
 
 @section('committee-content')
     <p>
-        Members of the adtie are the brilliant mind behind the Francken achievements. These are badges a member can earn on the website by displaying splendid behaviour, like doing the dishes or cleaning the fridge.
+        Members of the Adtie are the brilliant mind behind the Francken achievements. These are badges a member can earn on the website by displaying splendid behaviour, like doing the dishes or cleaning the fridge.
     </p>
 @endsection
