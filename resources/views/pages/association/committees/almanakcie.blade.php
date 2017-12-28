d993fa21b2e53da913395efe009b600ccf1895b0
SuStreepjeElle <30695375+SuStreepjeElle@users.noreply.github.com>
2017-12-28 14:27:54 +0100

    Update almanakcie.blade.php
    Added description

--- resources/views/pages/association/committees/almanakcie.blade.php
+++ resources/views/pages/association/committees/almanakcie.blade.php
@@ -2,6 +2,6 @@
 
     @section('committee-content')
     <p>
-        Almanakcie
+        It was the year 2013 when T.F.V. 'Professor Francken' won the Universiteits Krant Almanak Award. A group of brave members wanted to take a chance of making an even better almanak and not stop at their peak. In the year 2018, they will publish a whole new almanak. 
     </p>
 @endsection
