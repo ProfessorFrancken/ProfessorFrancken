633381e78d8f7863a5bb6f1e913cebc86f78cdab
Mark Redeman <markredeman@gmail.com>
2018-01-28 23:54:53 +0100

    Add option to add css stacks to admin pages

--- resources/views/admin/base-layout.blade.php
+++ resources/views/admin/base-layout.blade.php
@@ -15,6 +15,8 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.js" integrity="sha384-/y1Nn9+QQAipbNQWU65krzJralCnuOasHncUFXGkdwntGeSvQicrYkiUBwsgUqc1" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/contrib/auto-render.min.js" integrity="sha384-dq1/gEHSxPZQ7DdrM82ID4YVol9BYyU7GbWlIwnwyPzotpoc57wDw/guX8EaYGPx" crossorigin="anonymous"></script>
 
+        @stack('css')
+
         @include('layout._favicon')
     </head>
     <body>
