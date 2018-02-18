05ced4a877217b103d938367e65bb2943537f6ac
Mark Redeman <markredeman@gmail.com>
2018-02-18 23:58:22 +0100

    Add routes for boards and committees

--- src/Infrastructure/Http/routes.php
+++ src/Infrastructure/Http/routes.php
@@ -34,16 +34,67 @@ Route::group(['prefix' => 'api', 'middleware' => ['api']], function () {
 
         $db = DB::connection('francken-legacy');
         $selects = ['id', 'voornaam', 'initialen', 'tussenvoegsel', 'achternaam', 'geboortedatum', 'prominent', 'kleur', 'afbeelding', 'bijnaam', 'button_width', 'button_height'];
-        $members = $db->table('leden')->leftJoin('leden_extras', 'leden.id', 'leden_extras.lid_id')->select($selects)->where('is_lid', 1)->where('streeplijst', 'Afschrijven')->where('machtiging', 1)->whereNull('einde_lidmaatschap')->whereNull('deleted_at')->get();
+        $members = $db->table('leden')
+                 ->leftJoin('leden_extras', 'leden.id', 'leden_extras.lid_id')
+                 ->select($selects)->where('is_lid', 1)
+                 ->where('streeplijst', 'Afschrijven')
+                 ->where('machtiging', 1)
+                 ->whereNull('einde_lidmaatschap')
+                 ->whereNull('deleted_at')
+                 ->where('id', '<>', 1098) // filter out Guests
+                 ->get()
+                 ->map(function ($lid) {
+                     if ($lid->afbeelding !== null) {
+                         $lid->afbeelding = "https:/old.professorfrancken.nl/database/streep/afbeeldingen/{$lid->afbeelding}";
+                     }
+                     return $lid;
+                 });
 
         return collect(['members' => $members]);
 
         // file_put_contents(database_path(leden.json'), json_encode($leden));
     });
 
+    Route::get('boards', function () {
+        $db = DB::connection('francken-legacy');
+        $selects = ['lid_id', 'jaar', 'functie'];
+        $members = $db->table('commissie_lid')
+                 ->leftJoin('commissies', 'commissies.id', 'commissie_lid.commissie_id')
+                 ->select($selects)
+                 ->where('commissies.naam', 'Bestuur')
+                 ->get();
+
+        return collect(['boardMembers' => $members]);
+    });
+
+    Route::get('committees', function () {
+        $db = DB::connection('francken-legacy');
+        $selects = ['commissie_id', 'lid_id', 'jaar', 'functie', 'commissies.naam'];
+        $members = $db->table('commissie_lid')
+                 ->leftJoin('commissies', 'commissies.id', 'commissie_lid.commissie_id')
+                 ->select($selects)
+//                 ->where('jaar', '2016')
+                 ->get();
+
+        return collect(['committees' => $members]);
+    });
+
     Route::get('products', function () {
         $db = DB::connection('francken-legacy');
-        $products = $db->table('producten')->where('beschikbaar', 1)->leftJoin('producten_extras', 'producten.id', 'producten_extras.product_id')->get();
+        $products = $db->table('producten')
+                  ->where('beschikbaar', 1)
+                  ->leftJoin('producten_extras', 'producten.id', 'producten_extras.product_id')
+                  ->get()
+                  ->map(function ($product) {
+                      if ($product->afbeelding !== null) {
+                          $product->afbeelding = "https:/old.professorfrancken.nl/database/streep/afbeeldingen/{$product->afbeelding}";
+                      }
+                      if ($product->splash_afbeelding !== null) {
+                          $product->splash_afbeelding = "https:/old.professorfrancken.nl/database/streep/afbeeldingen/{$product->splash_afbeelding}";
+                      }
+                      return $product;
+                  });
+
         return collect(['products' => $products]);
     });
 
