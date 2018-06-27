5c62af3a2b8f476010732296ff7049bf979fef52
Mark Redeman <markredeman@gmail.com>
2018-06-27 16:54:43 +0200

    Move the menu used in NavigationServiceProvider into a config file

---
+++ config/francken/navigation.php
@@ -0,0 +1,157 @@
+<?php
+
+return [
+    'menu' => [
+        [
+            'url' => '/study',
+            'title' => 'Study',
+            'subItems' => [
+                ['url' => "/study/books", 'title' => 'Books'],
+                ['url' => "/study/research-groups", 'title' => 'Research Groups'],
+                ['url' => "/study/representation/university-council", 'title' => 'University Council'],
+                ['url' => "/study/representation/faculty-council", 'title' => 'Faculty Council'],
+            ],
+            'icon' => 'graduation-cap',
+        ],
+        [
+            'url' => '/association',
+            'title' => 'Association',
+            'subItems' => [
+                ['url' => "/association/news", 'title' => 'News'],
+                ['url' => "/association/history", 'title' => 'History'],
+                ['url' => "/association/honorary-members", 'title' => 'Honorary members'],
+                ['url' => "/association/boards", 'title' => 'Boards'],
+                ['url' => "/association/committees", 'title' => 'Committees'],
+                ['url' => "/association/francken-vrij", 'title' => 'Francken Vrij']
+            ],
+            //    'icon' => 'beer',
+            'icon' => 'coffee',
+        ],
+        [
+            'url' => '/career',
+            'title' => 'Career',
+            'subItems' => [
+                ['url' => "/career/job-openings", 'title' => 'Job openings'],
+                ['url' => "/career/companies", 'title' => 'Company profiles'],
+                ['url' => "/career/events", 'title' => 'Career events']
+            ],
+            'icon' => 'suitcase',
+        ],
+        [
+            'url' => 'https://www.flickr.com/photos/fotocie/sets/',
+            'title' => 'Photos',
+            'subItems' => [],
+            'icon' => 'camera',
+        ],
+    ],
+
+    'admin-menu' => [
+        [
+            "name" => "Association",
+            "url" => "association",
+            "items" => [
+                [
+                    "name" => "News",
+                    "url" => "news",
+                    "works" => true,
+                ],
+                [
+                    "name" => "Activities",
+                    "url" => "activities",
+                    "works" => false,
+                ],
+                [
+                    "name" => "Open registrations",
+                    "url" => "registration-requests",
+                    "works" => true,
+                ],
+                [
+                    "name" => "Members",
+                    "url" => "members",
+                    "works" => false,
+                ],
+                [
+                    "name" => "Committees",
+                    "url" => "committees",
+                    "works" => false,
+                ],
+                [
+                    "name" => "Francken Vrij",
+                    "url" => "francken-vrij",
+                    "works" => true,
+                ],
+            ]
+        ],
+        [
+            "name" => "Study",
+            "url" => "study",
+            "items" => [
+                [
+                    "name" => "Research Groups",
+                    "url" => "research-groups",
+                    "works" => false,
+                ],
+                [
+                    "name" => "Books",
+                    "url" => "books",
+                    "works" => false,
+                ],
+            ]
+        ], [
+            "name" => "Extern",
+            "url" => "extern",
+            "items" => [
+                [
+                    "name" => "Companies",
+                    "url" => "companies",
+                    "works" => false,
+                ],
+                [
+                    "name" => "Events",
+                    "url" => "events",
+                    "works" => false,
+                ],
+                [
+                    "name" => "Jop openings",
+                    "url" => "jop-openings",
+                    "works" => false,
+                ],
+                [
+                    "name" => "Factsheet",
+                    "url" => "fact-sheet",
+                    "works" => true,
+                ]
+            ]
+        ], [
+            "name" => "Committees",
+            "url" => "committees",
+            "items" => [
+                [
+                    "name" => "Adtcie",
+                    "url" => "adtcie",
+                    "works" => false,
+                ],
+                [
+                    "name" => "Borrelcie",
+                    "url" => "borrelcie",
+                    "works" => false,
+                ],
+                [
+                    "name" => "Francken Vrij",
+                    "url" => "francken-vrij",
+                    "works" => false,
+                ],
+                [
+                    "name" => "Brouwcie",
+                    "url" => "brouwcie",
+                    "works" => false,
+                ],
+                [
+                    "name" => "Fotocie",
+                    "url" => "fotocie",
+                    "works" => false,
+                ],
+            ]
+        ]
+    ]
+];
--- src/Infrastructure/NavigationServiceProvider.php
+++ src/Infrastructure/NavigationServiceProvider.php
@@ -7,65 +7,26 @@ use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 use DateTimeImmutable;
+use DateTimeZone;
 
 final class NavigationServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $items = [
-            [
-                'url' => '/study',
-                'title' => 'Study',
-                'subItems' => [
-                    ['url' => "/study/books", 'title' => 'Books'],
-                    ['url' => "/study/research-groups", 'title' => 'Research Groups'],
-                    ['url' => "/study/representation/university-council", 'title' => 'University Council'],
-                    ['url' => "/study/representation/faculty-council", 'title' => 'Faculty Council'],
-                ],
-                'icon' => 'graduation-cap',
-            ],
-            [
-                'url' => '/association',
-                'title' => 'Association',
-                'subItems' => [
-                    ['url' => "/association/news", 'title' => 'News'],
-                    ['url' => "/association/history", 'title' => 'History'],
-                    ['url' => "/association/honorary-members", 'title' => 'Honorary members'],
-                    ['url' => "/association/boards", 'title' => 'Boards'],
-                    ['url' => "/association/committees", 'title' => 'Committees'],
-                    ['url' => "/association/francken-vrij", 'title' => 'Francken Vrij']
-                ],
-                //    'icon' => 'beer',
-                'icon' => $this->associationIcon(),
-            ],
-            [
-                'url' => '/career',
-                'title' => 'Career',
-                'subItems' => [
-                    ['url' => "/career/job-openings", 'title' => 'Job openings'],
-                    ['url' => "/career/companies", 'title' => 'Company profiles'],
-                    ['url' => "/career/events", 'title' => 'Career events']
-                ],
-                'icon' => 'suitcase',
-            ],
-            [
-                'url' => 'https://www.flickr.com/photos/fotocie/sets/',
-                'title' => 'Photos',
-                'subItems' => [],
-                'icon' => 'camera',
-            ],
-        ];
+        View::composer('layout._header', function ($view) {
+            $menu = $this->app->config->get('francken.navigation.menu');
+            $menu[1]['icon'] = $this->associationIcon();
 
-        View::composer('layout._header', function ($view) use ($items) {
             if (Auth::check()) {
-                $items[] = [
+                $user = Auth::user();
+                $menu[] = [
                     'url' => '/my-francken',
                     'title' => 'My Francken',
                     'subItems' => [],
                     'icon' => 'user',
                 ];
             } else {
-                $items[] = [
+                $menu[] = [
                     'url' => '/login',
                     'title' => 'Login',
                     'subItems' => [],
@@ -74,19 +35,30 @@ final class NavigationServiceProvider extends ServiceProvider
                 ];
             }
 
-            $view->with('items', $items);
+            $view->with('items', $menu);
         });
 
-        View::composer('homepage._pillars', function ($view) use ($items) {
+        View::composer('admin.layout', function ($view) {
+            $menu = $this->app->config->get('francken.navigation.admin-menu');
+
+            $view->with('menu', $menu);
+        });
+
+        View::composer('homepage._pillars', function ($view) {
             $view->with('associationIcon', $this->associationIcon());
         });
     }
 
     public function associationIcon()
     {
-        $now = new DateTimeImmutable;
-        $fourOClock = DateTimeImmutable::createFromFormat('H a', '4 pm');
-        $fourOClockMorning = DateTimeImmutable::createFromFormat('H a', '4 am');
+        $now = (new DateTimeImmutable)
+            ->setTimeZone(new DateTimeZone('Europe/Amsterdam'));
+
+        $fourOClock = DateTimeImmutable::createFromFormat('H a', '4 pm')
+            ->setTimeZone(new DateTimeZone('Europe/Amsterdam'));
+
+        $fourOClockMorning = DateTimeImmutable::createFromFormat('H a', '4 am')
+            ->setTimeZone(new DateTimeZone('Europe/Amsterdam'));
 
         if ($fourOClockMorning < $now && $now < $fourOClock) {
             return 'coffee';
