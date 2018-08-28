ceb040c41d7ecb6729ceab58f79ddb886a74ddfd
Mark Redeman <markredeman@gmail.com>
2018-08-28 20:19:11 +0200

    Add a francken_id to users table

--- database/migrations/2017_02_11_213806_create_users_table.php
+++ database/migrations/2017_02_11_213806_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
 
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('francken_id');
             $table->rememberToken();
             $table->timestamps();
 
--- tests/Features/LoggedInAsAdmin.php
+++ tests/Features/LoggedInAsAdmin.php
@@ -20,6 +20,7 @@ trait LoggedInAsAdmin
 
             DB::table('users')->insert([
                 'id' => '1',
+                'francken_id' => 1,
                 'email' => 'board@professorfrancken.nl',
                 'password' => bcrypt($passphrase),
                 'can_access_admin' => true
