dbe3c4c6d13c542edac983a5c8eb90249408a933
Mark Redeman <markredeman@gmail.com>
2018-09-21 19:16:59 +0200

    Change starting and ending date of FactSheet data

--- src/Extern/Http/FactSheetController.php
+++ src/Extern/Http/FactSheetController.php
@@ -39,7 +39,7 @@ final class FactSheetController
             ->with([
                 'studies' => (new StudentsByStudyAndStudyPhase($this->today))->handle(),
                 'activeMembers' => (new ActiveMembersStatistics($this->today))->handle(),
-                'firstYearStudentsPerYear' => (new FirstYearStudentsPerYear(collect(range(2010, 2017))))->handle(),
+                'firstYearStudentsPerYear' => (new FirstYearStudentsPerYear(collect(range(2011, 2018))))->handle(),
                 'weeklyStats' => $this->weeklyStats(),
                 'monthlyStats' => $this->monthlyStats(),
             ]);
