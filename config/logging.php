334d2f039bf8aba3115adb3e9a0cae719362a4be
Mark Redeman <markredeman@gmail.com>
2018-11-18 20:27:00 +0100

    Setup daily logging by default

--- config/logging.php
+++ config/logging.php
@@ -32,7 +32,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily'],
         ],
 
         'single' => [
@@ -45,7 +45,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
-            'days' => 7,
+            'days' => 28,
         ],
 
         'slack' => [
