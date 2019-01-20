b9a2e413da92be8cde33db65941b190b0c3fb853
Mark Redeman <markredeman@gmail.com>
2019-01-20 19:27:23 +0100

    Add grayscale to footer images and align vertically to center

--- resources/views/layout/_sponsors.blade.php
+++ resources/views/layout/_sponsors.blade.php
@@ -6,8 +6,12 @@
     <div class="row">
         @foreach ($footer as $company)
             <div class="my-3 col-6 col-sm-4 col-md-3 col-lg-2 col-lx-1">
-                <a href="{{ $company['footer-link'] }}">
-                    <img class="img-fluid" src="{{ $company['footer-logo'] }}" alt="Logo of {{ $company['name'] }}">
+                <a href="{{ $company['footer-link'] }}" class="h-100 d-flex align-items-center">
+                    <img
+                        class="img-fluid"
+                        src="{{ $company['footer-logo'] }}"
+                        alt="Logo of {{ $company['name'] }}"
+                    >
                 </a>
             </div>
         @endforeach
