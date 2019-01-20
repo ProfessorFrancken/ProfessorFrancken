2ad8cfc82da10c41049330ee20a2d71747748cba
Mark Redeman <markredeman@gmail.com>
2019-01-20 22:53:20 +0100

    Apply code style fixes

--- resources/views/study/research-groups/show.blade.php
+++ resources/views/study/research-groups/show.blade.php
@@ -8,27 +8,26 @@
     </div>
     <div class="row mt-2">
 
-	      <div class="col text-justify " >
-
-		        {!!  $group['description'] !!}
-	      </div>
+          <div class="col text-justify">
+                {!! $group['description'] !!}
+          </div>
     </div>
     <br>
     <div class="row">
-	      @foreach($group['groups'] as $unit)
-		        <div class="col-md-6 mt-3">
-			          <img src="{{ image($unit['foto'], ['width' => 283, 'height' => 142]) }}" width="283" height="142" class="rounded">
+          @foreach($group['groups'] as $unit)
+                <div class="col-md-6 mt-3">
+                      <img src="{{ image($unit['foto'], ['width' => 283, 'height' => 142]) }}" width="283" height="142" class="rounded">
                         <h2> {{ $unit['group'] }} Group</h2>
-			          {{ $unit['title'] }}
-			          <br>
-			          <a class="btn btn-secondary" href="{{ $unit['contact'] }}">
-				            Contact
-			          </a>
-			          <a class="btn btn-info" href="{{ $unit['link'] }}">
-				            Group Info
-			          </a>
-		        </div>
-	      @endforeach
+                      {{ $unit['title'] }}
+                      <br>
+                      <a class="btn btn-secondary" href="{{ $unit['contact'] }}">
+                            Contact
+                      </a>
+                      <a class="btn btn-info" href="{{ $unit['link'] }}">
+                            Group Info
+                      </a>
+                </div>
+          @endforeach
     </div>
     <br><br><br>
 @endsection
