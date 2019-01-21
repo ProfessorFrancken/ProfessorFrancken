a9b3699f47817759c7dc333994de40846f0ce6df
Mark Redeman <markredeman@gmail.com>
2019-01-21 18:17:46 +0100

    Fix an height issue with the navigation menu

--- resources/views/layout/_header.blade.php
+++ resources/views/layout/_header.blade.php
@@ -1,7 +1,7 @@
 <header class="header d-flex" style="z-index: 1">
     @include("layout._logo")
     <div
-        class="d-none flex-md-grow-1 d-md-block bg-white navigation-container__wrapper"
+        class="d-none flex-md-grow-1 d-md-block bg-white navigation-container__wrapper h-100"
         style="
         box-shadow: 0 0px 5px rgba(0,0,0,0.2);
         z-index:1;"
