019267dc024673f894529d55043481d47427ee53
Mark Redeman <markredeman@gmail.com>
2019-02-17 23:55:49 +0100

    Add box-shadow to header image

--- resources/views/layout/header/_header_image.blade.php
+++ resources/views/layout/header/_header_image.blade.php
@@ -4,7 +4,7 @@
  }
 </style>
 <div class="stop-overflow">
-    <div class="header-image d-flex align-items-center justify-content-center">
+    <div class="header-image d-flex align-items-center justify-content-center" style="box-shadow: inset 0.5rem -0.5rem 1rem rgba(0,0,0,.125) !important">
 
         {{--
             Add a bit of margin to make the container in the header image align with
