a9e8f60494e39a245e456099af1621bb6ac4396a
Mark Redeman <markredeman@gmail.com>
2019-02-23 16:39:08 +0100

    Update login modal

--- resources/views/layout/_login-modal.blade.php
+++ resources/views/layout/_login-modal.blade.php
@@ -14,26 +14,69 @@
         </div>
 
         <div class="login-modal__content">
-            {!! Form::open(['url' => route('login')]) !!}
+<form method="POST" action="{{ route('login') }}">
+                @csrf
+
                 <div class="form-group">
-                    <h4 class="h5 login-modal__header text-right">
-                        <label for="email" class="login-modal__header text-right" id="Login_Modal_Title">
-                            Login
-                            <i class="login-modal__icon fa fa-user-o" aria-hidden="true"></i>
+                    <label for="email">Email</label>
+
+                    <input id="email"
+                           type="email"
+                           class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                           name="email"
+                           value="{{ old('email') }}"
+                           placeholder="member@professorfrancken.nl"
+                           required
+                           autofocus
+                    >
+
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="password">Passphrase</label>
+                    <input id="password"
+                           type="password"
+                           class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                           name="password"
+                           placeholder="Your password"
+                           required
+                    >
+
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                        <label class="form-check-label" for="remember">
+                            {{ __('Remember Me') }}
                         </label>
-                    </h4>
-                    {!! Form::email('email', null, ['placeholder' => 'Email', 'class' => 'form-control', 'id' => 'email']) !!}
-                </div>
-                <div class="form-group">
-                    {!! Form::password('password', ['placeholder' => 'A super  secret passphrase', 'class' => 'form-control', 'id' => 'passphrase']) !!}
+                    </div>
                 </div>
 
-                <div class="form-group text-right">
-                    <a class="btn btn-link" href="/register" style="color: white;">Register</a>
-                    <button type="submit" class="btn btn-inverse">Login</button>
-                </div>
-            {!! Form::close() !!}
+                <div class="form-group d-flex flex-column">
+                    <button type="submit" class="btn btn-inverse btn-lg btn-block btn-outline-primary">
+                        Login
+                    </button>
 
+                    @if (Route::has('password.request'))
+                        <a class="mt-2 text-white" href="{{ route('password.request') }}">
+                            <small>
+                            Forgot Your Password?
+                            </small>
+                        </a>
+                    @endif
+                </div>
+            </form>
             <p id="Modal_Description" class="visually-hidden">
                 Login with your Francken account.
             </p>
