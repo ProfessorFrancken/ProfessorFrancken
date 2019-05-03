ed6b5e45a5e9f42654e0bcb84988e1ed28c37501
Mark Redeman <markredeman@gmail.com>
2019-05-03 16:11:38 +0200

    Add role middleware
    This was needed for the birthdays route

--- src/Infrastructure/Http/Kernel.php
+++ src/Infrastructure/Http/Kernel.php
@@ -58,6 +58,7 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'guest' => \Francken\Infrastructure\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
