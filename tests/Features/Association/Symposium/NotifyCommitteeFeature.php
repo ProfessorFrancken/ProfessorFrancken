f16496fe5c2c15e7557ac2e9b193342bf7586580
Mark Redeman <markredeman@gmail.com>
2019-05-27 09:25:10 +0200

    Fix failing NotifyCommitteeFeature test

--- tests/Features/Association/Symposium/NotifyCommitteeFeature.php
+++ tests/Features/Association/Symposium/NotifyCommitteeFeature.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Francken\Features\Association\Symposium;
 
+use DateTimeImmutable;
 use Francken\Association\Symposium\Mail\NotifyCommittee as NotifyCommitteeMail;
 use Francken\Association\Symposium\NotifySymposiumCommittee;
-use Francken\Association\Symposium\Participant;
 use Francken\Association\Symposium\ParticipantRegisteredForSymposium;
 use Francken\Association\Symposium\Symposium;
 use Francken\Features\LoggedInAsAdmin;
@@ -22,11 +22,24 @@ class NotifyCommitteeFeature extends TestCase
     /** @test */
     public function it_sends_a_notification_email_to_the_committee() : void
     {
-        $symposium = new Symposium();
-        $participant = new Participant([
+        $symposium = Symposium::create([
+            'name' => 'In a materialistic world',
+            'start_date' => new DateTimeImmutable('05-05-2019 09:00'),
+            'end_date' => new DateTimeImmutable('05-05-2019 18:00'),
+            'location' => 'EM2',
+            'website_url' => 'https://franckensymposium.nl',
+            'open_for_registration' => true,
+            'promote_on_agenda' => true,
+        ]);
+        $participant =$symposium->participants()->create([
             'email' => 'markredeman@gmail.com',
             'firstname' => 'Mark',
             'lastname' => 'Redeman',
+            'is_francken_member' => true,
+            'is_nnv_member' => true,
+            'nnv_number' => '706116',
+            'payment_method'=> 'debit',
+            'iban' => 'NL18ABNA0484869868'
         ]);
 
         $event = new ParticipantRegisteredForSymposium($participant);
