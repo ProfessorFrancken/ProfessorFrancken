b2e854593ed5079ffc8e0a020307f5f5ea0e1779
Mark Redeman <markredeman@gmail.com>
2019-06-10 21:20:03 +0200

    Improve account activation notification emssage

--- resources/views/auth/mails/notify-about-account-activation.blade.php
+++ resources/views/auth/mails/notify-about-account-activation.blade.php
@@ -2,21 +2,17 @@
     {{-- Body --}}
 # Hi {{ $full_name }},
 
-You have been given an account which you can use to login to our website.
+You have been given an account which you can use to login to the website of our association [T.F.V. 'Professor Francken'](https://professorfrancken.nl).
+You can use this account to view your [latest expenses]({{ action([\Francken\Association\Members\Http\ExpensesController::class, 'index']}}) at our association and [view photos]({{ action([\Francken\Association\Photos\Http\Controllers\PhotosController::class, 'index']) }}) of activities.
+In the future we will add more features such as changing your personal information, registering online for activities managing your committees and more.
 
 ### Reset your password
 
-To login please first set password by clicking the button below.
+To login please first set a password by clicking the button below.
 Use your email, **{{ $email }}**.
 
 @component('mail::button', ['url' => $url])
     Set your password
 @endcomponent
-
-### What to do with your account
-
-- See your [latest expenses]({{ action([\Francken\Association\Members\Http\ExpensesController::class, 'index']) }})
-- See [all photos]({{ action([\Francken\Association\Photos\PhotosController::class, 'index']) }})
-- More features will be added in the future
 
 @endcomponent
