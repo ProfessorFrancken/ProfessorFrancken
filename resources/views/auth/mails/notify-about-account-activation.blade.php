0ee44b82c48d4d5912122edf6508737106e39b33
Mark Redeman <markredeman@gmail.com>
2019-06-20 11:59:03 +0200

    Fix notify account activation email

--- resources/views/auth/mails/notify-about-account-activation.blade.php
+++ resources/views/auth/mails/notify-about-account-activation.blade.php
@@ -3,7 +3,9 @@
 # Hi {{ $full_name }},
 
 You have been given an account which you can use to login to the website of our association [T.F.V. 'Professor Francken'](https://professorfrancken.nl).
-You can use this account to view your [latest expenses]({{ action([\Francken\Association\Members\Http\ExpensesController::class, 'index']}}) at our association and [view photos]({{ action([\Francken\Association\Photos\Http\Controllers\PhotosController::class, 'index']) }}) of activities.
+
+You can use this account to view your [latest expenses]({{ action([\Francken\Association\Members\Http\ExpensesController::class, 'index']) }}) at our association and [view photos]({{ action([\Francken\Association\Photos\Http\Controllers\PhotosController::class, 'index']) }}) of activities.
+
 In the future we will add more features such as changing your personal information, registering online for activities managing your committees and more.
 
 ### Reset your password
