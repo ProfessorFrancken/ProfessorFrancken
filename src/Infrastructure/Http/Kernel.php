0e40d784ccf61d1d33349a4f9bb52b3dbf5896c8
Mark Redeman <markredeman@gmail.com>
2019-06-30 17:17:06 +0200

    Add SubstituteBindings middleware to api

--- src/Infrastructure/Http/Kernel.php
+++ src/Infrastructure/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
         ],
         'api' => [
             \Francken\Infrastructure\Http\Middleware\EnableCORS::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
