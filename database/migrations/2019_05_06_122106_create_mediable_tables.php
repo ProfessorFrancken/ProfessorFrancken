c9a222588390620fd5923d4930e26741665339e9
Mark Redeman <markredeman@gmail.com>
2019-07-15 14:35:58 +0200

    Revert "Rename migration"
    This partially reverts commit 8a972fb1daddb629a644db7d96c105d5171313cf.
    
    We now keep the old migration file.

--- database/migrations/2019_05_06_122106_create_mediable_tables.php
+++ database/migrations/2019_05_06_122106_create_mediable_tables.php
@@ -1,15 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateMediableTables extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
---
+++ database/migrations/2019_05_27_122106_create_mediable_tables.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMediableTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
