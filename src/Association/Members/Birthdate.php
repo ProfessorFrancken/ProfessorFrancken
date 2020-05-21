15c99f42daab93263c20c4107c3ff985b5296ca0
Mark Redeman <markredeman@gmail.com>
2020-05-21 10:28:08 +0200

    Add Birthday VO

---
+++ src/Association/Members/Birthdate.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Francken\Association\Members;
+
+use DateTimeImmutable;
+use InvalidArgumentException;
+use Webmozart\Assert\Assert;
+
+final class Birthdate
+{
+    private $date;
+
+    private function __construct(DateTimeImmutable $date)
+    {
+        $this->date = $date;
+    }
+   
+    public function toString() : string
+    {
+        return $this->date->format('Y-m-d');
+    }
+
+    public function toDateTime() : DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    public function ageAt(DateTimeImmutable $time) : int
+    {
+        return $this->date->diff($time)->y;
+    }
+
+    public static function fromString(string $birthday) : self
+    {
+        $date = DateTimeImmutable::createFromFormat('!Y-m-d', $birthday);
+
+        Assert::isInstanceOf($date, DateTimeImmutable::class);
+
+        if ( ! $date) {
+            throw new InvalidArgumentException("Given date was not in Y-m-d format");
+        }
+
+        return self::fromDateTime($date);
+    }
+
+    public static function fromDateTime(DateTimeImmutable $date) : self
+    {
+        $birthdate = DateTimeImmutable::createFromFormat('!Y-m-d', $date->format('Y-m-d'));
+
+        Assert::isInstanceOf($birthdate, DateTimeImmutable::class);
+
+        return new self($birthdate);
+    }
+}
---
+++ tests/Francken/Association/Members/BirthdateTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Francken\Association\Members;
+
+use DateTimeImmutable;
+use Francken\Association\Members\Birthdate;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class BirthdateTest extends TestCase
+{
+    /** @test */
+    public function it_can_be_constructed_from_a_string() : void
+    {
+        $birthdate = Birthdate::fromString('2019-01-01');
+        $this->assertSame('2019-01-01', $birthdate->toString());
+    }
+
+    /** @test */
+    public function it_does_not_allow_invalid_formats_when_constructing_from_a_string() : void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Birthdate::fromString('01-2019-01');
+    }
+
+    /** @test */
+    public function it_can_be_constructed_from_a_datetime() : void
+    {
+        $birthdate = Birthdate::fromDateTime(
+            DateTimeImmutable::createFromFormat('Y-m-d', '2019-01-01')
+        );
+        $this->assertSame('2019-01-01', $birthdate->toString());
+    }
+
+    /** @test */
+    public function it_has_no_time_information_when_converting_to_datetime() : void
+    {
+        $birthdate = Birthdate::fromDateTime(
+            DateTimeImmutable::createFromFormat('Y-m-d', '2019-01-01')
+        );
+
+        $this->assertEquals(
+            '2019-01-01T00:00:00+00:00', $birthdate->toDateTime()->format(DateTimeImmutable::ATOM)
+        );
+    }
+
+    /** @test */
+    public function it_finds_the_age_of_a_person_at_a_given_date() : void
+    {
+        $birthdate = Birthdate::fromDateTime(
+            DateTimeImmutable::createFromFormat('Y-m-d', '1993-01-01')
+        );
+
+        $this->assertEquals(
+            26,
+            $birthdate->ageAt(DateTimeImmutable::createFromFormat('Y-m-d', '2019-01-01'))
+        );
+    }
+}
