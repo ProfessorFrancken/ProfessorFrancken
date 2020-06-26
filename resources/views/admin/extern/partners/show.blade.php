8da11d00774c99ea69d7023183bc4f7bc30145ba
Mark Redeman <markredeman@gmail.com>
2020-06-26 12:58:20 +0200

    Extract notes partial view

--- resources/views/admin/extern/partners/show.blade.php
+++ resources/views/admin/extern/partners/show.blade.php
@@ -104,57 +104,7 @@
             {!! Form::close() !!}
         </div>
         <div class="col-3">
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="font-weight-bold">
-                        <i class="fas fa-sticky-note"></i>
-                        Extern notes
-                    </h4>
-
-                    <ul class='list-unstyled'>
-                        @forelse ($partner->notes as $note)
-                            <li class="d-flex flex-column {{ $loop->last ? '' : 'border-bottom  my-3 py-3' }}">
-                                <small class="text-muted">
-                                    On {{ $note->created_at->format('Y-m-d') }}, {{ $note->member->fullname }} wrote:
-                                </small>
-                                <p class="bg-light p-3 my-1">
-                                    {{ $note->note }}
-                                </p>
-                            </li>
-                        @empty
-                            <li class="d-flex flex-column my-3">
-                                Use these notes to keep to keep track of arrangements with a partner or any other information that might be useful for you or your future kandi.
-                            </li>
-                        @endforelse
-                    </ul>
-                </div>
-                <div class="card-footer">
-                    {!!
-                       Form::model($partner, [
-                           'url' => action(
-                               [\Francken\Extern\Http\AdminPartnerNotesController::class, 'store'],
-                               ['partner' => $partner]
-                           ),
-                           'method' => 'POST',
-                       ])
-                    !!}
-                    <div class="form-group">
-                        {!!
-                           Form::textarea(
-                               'note',
-                               null,
-                               ['class' => 'form-control', 'id' => 'note', 'rows' => 3]
-                           )
-                        !!}
-                    </div>
-                    <button class='btn btn-text btn-sm'>
-                        <i class="fas fa-check"></i>
-                        Save note
-                    </button>
-                    {!! Form::close() !!}
-                </div>
-            </div>
-
+            @include('admin.extern.partners._notes', ['partner' => $partner])
             @include('admin.extern.partners._contacts', ['partner' => $partner])
 
             @if (1 == 2)
