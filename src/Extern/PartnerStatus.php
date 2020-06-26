f71aae032a9388f6b3d10be672c0eb27bf138830
Mark Redeman <markredeman@gmail.com>
2020-06-26 15:33:26 +0200

    Add additional partner statuses

--- src/Extern/PartnerStatus.php
+++ src/Extern/PartnerStatus.php
@@ -6,6 +6,8 @@ namespace Francken\Extern;
 
 final class PartnerStatus
 {
+    public const PRIMARY_PARTNER = 'main_partner';
+    public const SECONDARY_PARTNER = 'secondary_partner';
     public const ACTIVE_PARTNER = 'active_partner';
     public const POTENTIAL_PARTNER = 'potential_partner';
     public const PAST_PARTNER = 'past_partner';
