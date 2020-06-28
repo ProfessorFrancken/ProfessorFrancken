5a9f2d0590c2377edc17fe78ed2196da0ff067e1
Mark Redeman <markredeman@gmail.com>
2020-06-28 10:42:04 +0200

    Eager load partner's sector

--- src/Extern/Http/AdminPartnersController.php
+++ src/Extern/Http/AdminPartnersController.php
@@ -61,6 +61,7 @@ final class AdminPartnersController
                 }
             })
             ->with([
+                'sector',
                 'logoMedia',
                 'companyProfile',
                 'footer',
