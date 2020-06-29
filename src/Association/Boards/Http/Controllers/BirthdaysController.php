72f126162df6ca67e466ba96277f222f4ca8f6a0
Mark Redeman <markredeman@gmail.com>
2020-06-29 21:40:47 +0200

    Derive board members from BoardMember

--- src/Association/Boards/Http/Controllers/BirthdaysController.php
+++ src/Association/Boards/Http/Controllers/BirthdaysController.php
@@ -6,6 +6,8 @@ namespace Francken\Association\Boards\Http\Controllers;
 
 use DateInterval;
 use DateTimeImmutable;
+use Francken\Association\Boards\BoardMember;
+use Francken\Shared\Clock\Clock;
 use Illuminate\Database\DatabaseManager;
 
 final class BirthdaysController
@@ -18,50 +20,40 @@ final class BirthdaysController
                       ->table('commissie_lid');
     }
 
-    public function index()
+    public function index(Clock $clock)
     {
-        $today = (new DateTimeImmutable())->sub(new DateInterval('P1D'));
+        $today = $clock->now()->sub(new DateInterval('P1D'));
 
-        $members = $this->boards
-                 ->select(['leden.voornaam', 'leden.tussenvoegsel', 'leden.achternaam', 'leden.geboortedatum'])
-                 ->join('leden', 'commissie_lid.lid_id', '=', 'leden.id')
-                 ->where('commissie_lid.commissie_id', 14)
-                 ->orderByRaw('DAYOFYEAR(leden.geboortedatum)')
-                 ->get()
-                 ->filter(function ($member) {
-                     return $member->geboortedatum !== null;
-                 })
-                 ->map(function ($member) {
-                     return [
-                         'name' => implode(
-                             ' ',
-                             array_filter([$member->voornaam, $member->tussenvoegsel, $member->achternaam])
-                         ),
-                         'birthday' => new DateTimeImmutable($member->geboortedatum)
-                     ];
-                 })->map(function ($member) use ($today) {
-                     $birthdayInSameYearAsNow = $member['birthday']->setDate(
-                         (int)$today->format('Y'),
-                         (int)$member['birthday']->format('m'),
-                         (int)$member['birthday']->format('d')
-                     );
+        $members = BoardMember::with(['member'])->get()->filter(function (BoardMember $member) : bool {
+            return $member->member !== null && $member->member->geboortedatum !== null;
+        })->map(function (BoardMember $member) {
+            return [
+                'name' => $member->member->full_name,
+                'birthday' => new DateTimeImmutable($member->member->geboortedatum)
+            ];
+        })->map(function ($member) use ($today) {
+            $birthdayInSameYearAsNow = $member['birthday']->setDate(
+                (int) $today->format('Y'),
+                (int) $member['birthday']->format('m'),
+                (int) $member['birthday']->format('d')
+            );
 
-                     $birthday = $birthdayInSameYearAsNow->setDate(
-                         (int)$birthdayInSameYearAsNow->format('Y') + ($today > $birthdayInSameYearAsNow ? 1 : 0),
-                         (int)$birthdayInSameYearAsNow->format('m'),
-                         (int)$birthdayInSameYearAsNow->format('d')
-                     );
+            $birthday = $birthdayInSameYearAsNow->setDate(
+                (int) $birthdayInSameYearAsNow->format('Y') + ($today > $birthdayInSameYearAsNow ? 1 : 0),
+                (int) $birthdayInSameYearAsNow->format('m'),
+                (int) $birthdayInSameYearAsNow->format('d')
+            );
 
-                     return array_merge($member, ['day' => $birthday]);
-                 })->sortBy(function ($member) use ($today) {
-                     return ($member['day']->getTimestamp() - $today->getTimestamp());
-                 })->groupBy(function ($member) {
-                     return $member['day']->format('Y');
-                 })->map(function ($year) {
-                     return $year->groupBy(function ($member) {
-                         return $member['day']->format('F');
-                     });
-                 });
+            return array_merge($member, ['day' => $birthday]);
+        })->sortBy(function ($member) use ($today) {
+            return ($member['day']->getTimestamp() - $today->getTimestamp());
+        })->groupBy(function ($member) {
+            return $member['day']->format('Y');
+        })->map(function ($year) {
+            return $year->groupBy(function ($member) {
+                return $member['day']->format('F');
+            });
+        });
 
         return view('association.boards.birthdays', [
             'years' => $members, 'today' => new DateTimeImmutable(),
