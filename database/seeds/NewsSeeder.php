398284ebedc44285249e586e9a060827dd1c4b81
Mark Redeman <markredeman@gmail.com>
2020-07-02 10:53:40 +0200

    Add news seeder

---
+++ database/seeds/NewsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Faker\Factory;
+use Francken\Association\News\Fake\FakeNews;
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run() : void
+    {
+        $faker = Factory::create();
+        $faker->seed(31415);
+        $fakeNews = (new FakeNews($faker, 30))->all();
+
+        foreach ($fakeNews as $news) {
+            $news->save();
+        }
+    }
+}
