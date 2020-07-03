6979b8bfb3dce59919264f37c0d065b9fb863309
Mark Redeman <markredeman@gmail.com>
2020-07-03 18:29:42 +0200

    Don't show a random adorable avatar

--- resources/views/committees/_members.blade.php
+++ resources/views/committees/_members.blade.php
@@ -2,22 +2,13 @@
     Committee members
 </h3>
 
-<div class="row">
+<ul>
     @foreach ($members as $member)
-        <div class="col-12 col-sm-6 my-3">
-            <div class="media">
-                <img
-                    class="committee-member__photo"
-                    src="https://api.adorable.io/avatars/75/{{ $member->member_id }}.png"
-                    alt="Photo of {{ $member->member->full_name }}"
-                >
-                <div class="align-self-center">
-                    <h5>
-                        {{ $member->member->full_name }}
-                    </h5>
-                </div>
-            </div>
-        </div>
+        <li>
+            <h5>
+                {{ $member->member->full_name }}
+            </h5>
+        </li>
     @endforeach
-</div>
+</ul>
 <hr/>
