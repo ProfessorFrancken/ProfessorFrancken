56943e7f71e6ae2e9e9037f15dd7e350bade7c9e
Mark Redeman <markredeman@gmail.com>
2020-07-12 11:58:01 +0200

    Move events into members namespace

--- src/Association/Members/Events/MemberEmailWasChanged.php
+++ src/Association/Members/Events/MemberEmailWasChanged.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Francken\Association\Events;
+namespace Francken\Association\Members\Events;
 
 use Francken\Association\LegacyMember;
 use Francken\Association\Members\Email;
