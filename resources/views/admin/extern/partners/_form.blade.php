dfa7a2b72ab7d7792134348b2fafa472eb259766
Mark Redeman <markredeman@gmail.com>
2020-07-14 19:37:36 +0200

    Improve partner form

--- resources/views/admin/extern/partners/_form.blade.php
+++ resources/views/admin/extern/partners/_form.blade.php
@@ -9,7 +9,22 @@
             <label for="sector_id">Sector</label>
             {!! Form::select('sector_id', $sectors, null, ['class' =>'form-control', 'id' => 'sector_id']) !!}
         </div>
+    </div>
+    <div class="col">
+        <div class="form-group">
+            <label for="status">Partner status</label>
+            {!! Form::select('status', $statuses, null, ['class' =>'form-control', 'id' => 'status']) !!}
+        </div>
 
+        <div class="form-group">
+            <label for="last_contract_ends_at">(Last) Contract ends at</label>
+            {!! Form::date('last_contract_ends_at', optional($partner->last_contract_ends_at)->format('Y-m-d'), ['class' =>'form-control', 'id' => 'last_contract_ends_at']) !!}
+            <small class="form-text text-muted">
+                Use this field to keep track of partners with an active contract.
+            </small>
+        </div>
+    </div>
+    <div class="col">
         <div class="form-group">
             <label for="homepage_url">Homepage url</label>
             {!! Form::text('homepage_url', null, ['class' => 'form-control', 'placeholder' => 'https://scriptcie.nl', 'id' => 'homepage_url']) !!}
@@ -22,19 +37,6 @@
             <small  class="form-text text-muted">
                 The refferal url is used when linking to the partner's website in our footer, company profile etc.
                 Partners can use this to determine the amount of trafick that they receive from out website.
-            </small>
-        </div>
-
-        <div class="form-group">
-            <label for="status">Partner status</label>
-            {!! Form::select('status', $statuses, null, ['class' =>'form-control', 'id' => 'status']) !!}
-        </div>
-
-        <div class="form-group">
-            <label for="last_contract_ends_at">(Last) Contract ends at</label>
-            {!! Form::date('last_contract_ends_at', optional($partner->last_contract_ends_at)->format('Y-m-d'), ['class' =>'form-control', 'id' => 'last_contract_ends_at']) !!}
-            <small class="form-text text-muted">
-                Use this field to keep track of partners with an active contract.
             </small>
         </div>
     </div>
