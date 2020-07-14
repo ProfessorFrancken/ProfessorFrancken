661fcfbf387e7150e8721eef0160eec36688afc4
Mark Redeman <markredeman@gmail.com>
2020-07-14 22:36:33 +0200

    Add smoke test for factsheet

---
+++ tests/Features/Extern/FactsheetFeature.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Francken\Features\Extern;
+
+use Francken\Association\Boards\Board;
+use Francken\Extern\Http\FactSheetController;
+use Francken\Features\LoggedInAsAdmin;
+use Francken\Features\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Http\UploadedFile;
+
+class FactsheetFeature extends TestCase
+{
+    use DatabaseMigrations;
+    use LoggedInAsAdmin;
+
+    /** @test */
+    public function a_factsheet_is_shown() : void
+    {
+        $board = factory(Board::class)->create(['installed_at' => '2020-06-06']);
+        $this->visit(action([FactSheetController::class, 'index']));
+
+        $this->assertResponseOk();
+    }
+}
+
