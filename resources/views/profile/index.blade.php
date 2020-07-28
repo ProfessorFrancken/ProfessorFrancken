d7ee52d65c8213c08a7d2042c71a6424ae2eda26
Mark Redeman <markredeman@gmail.com>
2020-07-28 10:45:57 +0200

    Show edit contact details link

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -24,7 +24,7 @@
 
     <div class="card my-3">
         <div class="card-body">
-            <a href="" class="float-right text-muted d-none">
+            <a href="{{ action([\Francken\Association\Members\Http\ContactDetailsController::class, 'index'])}}" class="float-right text-muted">
                 <i class="fas fa-edit"></i>
                 Edit
             </a>
