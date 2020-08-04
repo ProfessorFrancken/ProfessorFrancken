8e3920d8372f8990a16f42c53355e3e0928ffdda
Mark Redeman <markredeman@gmail.com>
2020-08-04 16:59:59 +0200

    Add initial activities feature test

---
+++ tests/Features/Association/Activities/AdminActivitiesFeature.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Francken\Features\Association\Committees;
+
+use Francken\Association\Activities\Activity;
+use Francken\Association\Activities\Http\AdminActivitiesController;
+use Francken\Features\LoggedInAsAdmin;
+use Francken\Features\TestCase;
+
+class AdminActivitiesFeature extends TestCase
+{
+    use LoggedInAsAdmin;
+
+    /** @test */
+    public function a_list_of_activities_is_shown() : void
+    {
+        $activity = factory(Activity::class)->create();
+
+        $this->visit(action([AdminActivitiesController::class, 'index']))
+            ->see($activity->name);
+
+        $this->assertResponseOk();
+    }
+}
