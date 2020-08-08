f62c8020cf57b55e09051a346069abb596f5a750
Mark Redeman <markredeman@gmail.com>
2020-08-08 15:43:48 +0200

    Remove login is disabled warning

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,9 +4,6 @@
 @section('content')
     <div class="d-flex justify-content-center">
         <div class="card p-5 mx-3 mx-md-5 w-100">
-            <div class="alert alert-warning">
-                <strong>Login is currently disabled.</strong>
-            </div>
             <h1 class="section-header">
                 <i class="fa fa-user" aria-hidden="true"></i>
                 Login
