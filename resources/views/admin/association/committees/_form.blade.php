a95f1333b393aa133179ea064fcc3772decb87f0
Mark Redeman <markredeman@gmail.com>
2020-08-10 20:06:34 +0200

    Extract duplicated markdown forms to blade component

--- resources/views/admin/association/committees/_form.blade.php
+++ resources/views/admin/association/committees/_form.blade.php
@@ -94,31 +94,7 @@
             </div>
         </div>
 
-        <div class="row d-flex align-items-stretch">
-            <div class="col">
-                <div class="form-group">
-                    <label for="source_content">Content</label>
-                    {!!
-                       Form::textarea(
-                           'source_content',
-                           null,
-                           ['class' => 'form-control', 'id' => 'source_content']
-                       )
-                    !!}
-                    <small class="form-text text-muted">
-                        Use <a href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet" target="_blank">
-                        Markdown</a> to format the committee page.
-                    </small>
-                </div>
-            </div>
-            <div class="col-md-6 d-none">
-                <div style="overflow-y: scroll" class="card">
-                    <div class="card-body" id="preview">
-                        {!! $committee->compiled_content !!}
-                    </div>
-                </div>
-            </div>
-        </div>
+        <x-forms.markdown />
     </div>
 </div>
 
@@ -164,52 +140,4 @@
      });
  })()
 </script>
-@endpush
-
-
-@push('scripts')
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
-<script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
-{{-- Syntax highlighting --}}
-<script src="https://cdn.jsdelivr.net/highlight.js/latest/highlight.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/highlight.js/latest/styles/github.min.css">
-<script>
- var simplemde = new SimpleMDE({
-     element: document.getElementById("source_content"),
-     spellChecker: false,
-     promptURLs: true,
-     /* previewRender: ,*/
-     /* previewRender: function(plainText) {*/
-     /* console.log(plainText);*/
-     /* return customMarkdownParser(plainText); // Returns HTML from a custom parser*/
-     /* },*/
-     previewRender: function(plainText) { // Async method
-         var that = this;
-         var parent = that.parent;
-
-         var preview = document.getElementById("preview");
-         var compiled = this.parent.markdown(plainText);
-         preview.innerHTML = compiled;
-         return compiled;
-     },
-
-     toolbar: [
-         "bold", "italic", "strikethrough",
-         "|",
-         "heading-1", "heading-2", "heading-3",
-         "|",
-         "code", "quote", "link", "image", "table",
-         "|",
-         "unordered-list", "ordered-list",
-         "|",
-         "preview", "side-by-side", "fullscreen",
-         "guide"
-     ]
- });
-</script>
-<style type="text/css">
- .CodeMirror, .CodeMirror-scroll {
-     max-height: 300px;
- }
-</style>
 @endpush
