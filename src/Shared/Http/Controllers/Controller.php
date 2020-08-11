7dfd5a1a4d03a46d7f4c0695d8c0d96e45a29bdc
Mark Redeman <markredeman@gmail.com>
2020-08-11 22:16:17 +0200

    Remove unused files

--- src/Shared/Http/Controllers/Controller.php
+++ src/Shared/Http/Controllers/Controller.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Francken\Shared\Http\Controllers;
 
-use Francken\Shared\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use DispatchesCommands;
 }
