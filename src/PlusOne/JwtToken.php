d8b7e0c630dcd9e1d68ae9d6abf5474c4aadd32b
Mark Redeman <markredeman@gmail.com>
2020-08-15 11:55:04 +0200

    Extract JwtToken

---
+++ src/PlusOne/JwtToken.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Francken\PlusOne;
+
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Token;
+
+class JwtToken
+{
+    private string $key;
+
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+    }
+
+    public function token() : Token
+    {
+        $now = time();
+
+        $signer = new Sha256();
+
+        return (new Builder())
+               ->setIssuedAt($now)
+               ->setExpiration($this->expiration($now))
+               ->set('plus-one', true)
+               ->sign($signer, $this->key)
+               ->getToken();
+    }
+
+    private function expiration(int $now) : int
+    {
+        // Currently set the expiration date to next year, later we could change
+        // this to be dependent on the user who's trying to login
+        return $now + 3600 * 24 * 365;
+    }
+}
