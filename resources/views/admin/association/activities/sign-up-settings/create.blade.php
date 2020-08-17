b3a62a5f12b26d642eb8c5ba8a3f74552dc6f71b
Mark Redeman <markredeman@gmail.com>
2020-08-17 20:41:17 +0200

    Don't use <x-forms.form> component

--- resources/views/admin/association/activities/sign-up-settings/create.blade.php
+++ resources/views/admin/association/activities/sign-up-settings/create.blade.php
@@ -4,17 +4,18 @@
 @section('content')
     <div class="card">
         <div class="card-body">
-            <x-forms.form
-                :model="$activity"
-                :url="action(
-                    [\Francken\Association\Activities\Http\AdminSignUpSettingsController::class, 'store'],
-                    ['activity' => $activity]
-                )"
-            >
+            {!!
+                   Form::model($activity, [
+                       'url' => action(
+                           [\Francken\Association\Activities\Http\AdminSignUpSettingsController::class, 'store'] ,
+                           ['activity' => $activity]
+                       ),
+                   ])
+            !!}
                 @include('admin.association.activities.sign-up-settings._form', ['committee' => $activity])
 
                 <x-forms.submit>Add sign up settings</x-forms.submit>
-            </x-forms.form>
+            {!! Form::close() !!}
         </div>
     </div>
 @endsection
