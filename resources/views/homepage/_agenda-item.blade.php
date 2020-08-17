05b0f37d57be52c8fc3c5a14ec223edeb650299d
Mark Redeman <markredeman@gmail.com>
2020-08-17 21:52:48 +0200

    Show sign up information on frontpage

--- resources/views/homepage/_agenda-item.blade.php
+++ resources/views/homepage/_agenda-item.blade.php
@@ -1,5 +1,5 @@
 <li class="agenda-item">
-    <div class=" d-flex d-flex align-items-center">
+    <div class=" d-flex d-flex align-items-start">
         <div class="agenda-item__date align-self-start">
             <h5 class="agenda-item__date-day">{{ $activity->start_date->format('d') }}</h5>
             <span class="agenda-item__date-month">{{ $activity->start_date->format('M') }}</span>
@@ -11,17 +11,51 @@
                             ['activity' => $activity]
                         ) }}">
                 <h5 class="agenda-item__header">{{ $activity->name }}</h5>
-                <p class="agenda-item__description">
-                    {{ $activity->summary }}
-                </p>
-                <small class="mt-1 text-muted font-weight-light d-block">
-                    <i class="fas fa-clock"></i>
-                    {{ $activity->schedule }}
-                </small>
-                @if ($activity->location !== '')
-                    <small class="mt-1 text-muted font-weight-light">
-                        <i class="fas fa-map-marker"></i>
-                        {{ $activity->location  }}
+                <div class="d-flex align-items-center mt-2 mb-1">
+                    @if ($activity->signUpSettings !== null)
+                        @if ($activity->signUpSettings->is_free)
+                            <small class="text-muted">
+                                <i class="fas fa-euro-sign"></i>
+                                Free
+                            </small>
+                            @else
+                            <small class="text-muted font-weight-light">
+                                <i class="fas fa-euro-sign"></i>
+                                {{ number_format($activity->signUpSettings->costs_per_person / 100, 2) }}
+                            </small>
+                        @endif
+
+                            <small class="text-muted font-weight-light mx-2" style="font-size: 0.7rem">
+                                •
+                            </small>
+                        <small class="text-muted font-weight-light">
+                            <i class="fas fa-users mr-1"></i>
+
+                            {{ $activity->totalSignUps }} /
+
+                            @if ($activity->signUpSettings->max_sign_ups === null)
+                                ∞
+                            @else
+                                {{ $activity->signUpSettings->max_sign_ups }}
+                            @endif
+                        </small>
+                    @endif
+                    @if ($activity->signUpSettings !== null && $activity->comments_count > 0)
+                        <small class="text-muted font-weight-light mx-2" style="font-size: 0.7rem">
+                            •
+                        </small>
+                    @endif
+                    @if ($activity->comments_count > 0)
+                        <small class="text-muted font-weight-light">
+                            <i class="fas fa-comments"></i>
+                            {{ $activity->comments_count  }}
+                        </small>
+                    @endif
+                </div>
+                @if ($activity->signUpSettings !== null)
+                    <small class="text-muted font-weight-light">
+                        <i class="fas fa-clock"></i>
+                        Registration ends in {{ $activity->signUpSettings->deadline_at->diffForHumans() }}
                     </small>
                 @endif
             </a>
