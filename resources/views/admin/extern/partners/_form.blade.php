f9af556c4c79be60fe65a449501e5c50cbad28a8
Mark Redeman <markredeman@gmail.com>
2020-08-17 13:35:30 +0200

    Extract partner form components

--- resources/views/admin/extern/partners/_form.blade.php
+++ resources/views/admin/extern/partners/_form.blade.php
@@ -1,44 +1,28 @@
 <div class="row">
     <div class="col">
-        <div class="form-group">
-            <label for="name">Name</label>
-            {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'S[ck]rip(t|t?c)ie In[ck]', 'id' => 'name']) !!}
-        </div>
-
-        <div class="form-group">
-            <label for="sector_id">Sector</label>
-            {!! Form::select('sector_id', $sectors, null, ['class' =>'form-control', 'id' => 'sector_id']) !!}
-        </div>
+        <x-forms.text name="name" label="Name" placeholder="S[ck]rip(t|t?c)ie In[ck]" />
+        <x-forms.select name="sector_id" label="Sector" :options="$sectors" />
     </div>
     <div class="col">
-        <div class="form-group">
-            <label for="status">Partner status</label>
-            {!! Form::select('status', $statuses, null, ['class' =>'form-control', 'id' => 'status']) !!}
-        </div>
-
-        <div class="form-group">
-            <label for="last_contract_ends_at">(Last) Contract ends at</label>
-            {!! Form::date('last_contract_ends_at', optional($partner->last_contract_ends_at)->format('Y-m-d'), ['class' =>'form-control', 'id' => 'last_contract_ends_at']) !!}
-            <small class="form-text text-muted">
+        <x-forms.select name="status" label="Partner status" :options="$statuses" />
+        <x-forms.date
+            name="last_contract_ends_at"
+            label="(Last) Contract ends at"
+            :value="optional($partner->last_contract_ends_at)->format('Y-m-d')"
+        >
+            <x-slot name="help">
                 Use this field to keep track of partners with an active contract.
-            </small>
-        </div>
+            </x-slot>
+        </x-forms.date>
     </div>
     <div class="col">
-        <div class="form-group">
-            <label for="homepage_url">Homepage url</label>
-            {!! Form::text('homepage_url', null, ['class' => 'form-control', 'placeholder' => 'https://scriptcie.nl', 'id' => 'homepage_url']) !!}
-        </div>
-
-        <div class="form-group">
-            <label for="referral_url">Referral url</label>
-            {!! Form::text('referral_url', null, ['class' => 'form-control', 'placeholder' => 'https://scriptcie.nl', 'id' => 'referral_url']) !!}
-
-            <small  class="form-text text-muted">
+        <x-forms.text name="homepage_url" label="Homepage url" placeholder="https://scriptcie.nl" />
+        <x-forms.text name="referral_url" label="Referral url" placeholder="https://scriptcie.nl">
+            <x-slot name="help">
                 The refferal url is used when linking to the partner's website in our footer, company profile etc.
                 Partners can use this to determine the amount of trafick that they receive from out website.
-            </small>
-        </div>
+            </x-slot>
+        </x-forms.text>
     </div>
     <div class="col">
         <div class="d-flex flex-column justify-content-end h-100">
