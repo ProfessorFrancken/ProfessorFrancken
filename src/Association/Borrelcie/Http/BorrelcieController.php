f9bfbdf8739ca69faabd4b97738cbf8d191a1903
Mark Redeman <markredeman@gmail.com>
2020-08-25 22:33:29 +0200

    Add breadcrumbs

--- src/Association/Borrelcie/Http/BorrelcieController.php
+++ src/Association/Borrelcie/Http/BorrelcieController.php
@@ -10,6 +10,11 @@ final class BorrelcieController
 {
     public function index() : View
     {
-        return view('association.borrelcie.index');
+        return view('association.borrelcie.index')->with([
+            'breadcrumbs' => [
+                ['url' => action([self::class, 'index']), 'text' => 'Borrelcie'],
+                ['url' => action([self::class, 'index']), 'text' => 'Activate account'],
+            ],
+        ]);
     }
 }
