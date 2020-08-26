55b9875596027ba6338af344615b91cbce24d22b
Mark Redeman <markredeman@gmail.com>
2020-08-26 11:28:45 +0200

    Refactor autocomplete-member component
    This makes it possible to complete other type of members

--- resources/views/components/forms/autocomplete-member.blade.php
+++ resources/views/components/forms/autocomplete-member.blade.php
@@ -25,23 +25,20 @@
 <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
 @endpush
 
+@push('styles')
+<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+@endpush
+
 @push('scripts')
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 <script type="text/javascript">
  $(document).ready(function () {
-     var members = {!! json_encode($members) !!};
-     var membersSource = members.map(function (member) {
-         return {
-             label: [member.voornaam, member.tussenvoegsel, member.achternaam].filter(function (val) { return val }).join(' '),
-             id: member.id
-         };
-     });
+     var membersSource = {!! json_encode($members) !!};
 
      $('#{{ $name }}').autocomplete({
          source: membersSource,
          select: function (event, ui) {
-             console.log($('#{{ $nameId }}'));
              $('#{{ $nameId }}').val(ui.item.id);
          },
          minLength: 2
