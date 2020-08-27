e3e4abcbdbc54b36cffa6434063e9b21cd68a1af
Mark Redeman <markredeman@gmail.com>
2020-08-27 16:29:32 +0200

    Refactor BorrelcieAccount
    Get the account via the Account model to make use of caching.

--- src/Association/Borrelcie/BorrelcieAccount.php
+++ src/Association/Borrelcie/BorrelcieAccount.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Francken\Association\Borrelcie;
 
 use Francken\Association\LegacyMember;
-use Francken\Auth\Account;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -28,11 +26,6 @@ final class BorrelcieAccount extends Model
     protected $casts = [
         'member_id' => 'int'
     ];
-
-    public function scopeOfAccount(Builder $query, Account $account) : Builder
-    {
-        return $query->where('member_id', $account->member_id);
-    }
 
     public function member() : BelongsTo
     {
--- src/Association/Borrelcie/Http/Middleware/HasBorrelcieAccount.php
+++ src/Association/Borrelcie/Http/Middleware/HasBorrelcieAccount.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Francken\Association\Borrelcie\Http\Middleware;
 
 use Closure;
-use Francken\Association\Borrelcie\BorrelcieAccount;
 use Francken\Association\Borrelcie\Http\BorrelcieAccountActivationController;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,9 +16,9 @@ final class HasBorrelcieAccount
      */
     public function handle(Request $request, Closure $next) : Response
     {
-        $borrelcieAccount = BorrelcieAccount::ofAccount($request->user())->first();
+        $account = $request->user()->borrelcieAccount()->first();
 
-        return $borrelcieAccount === null
+        return $account === null
             ? redirect()->action([BorrelcieAccountActivationController::class, 'index'])
             : $next($request);
     }
