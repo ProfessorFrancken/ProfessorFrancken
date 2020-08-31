d0c2fe01d97f8d72ad445989b3a6cee82bfd4b83
Mark Redeman <markredeman@gmail.com>
2020-08-31 16:55:02 +0200

    Refactor agenda

--- resources/views/homepage/_agenda-item.blade.php
+++ resources/views/homepage/_agenda-item.blade.php
@@ -1,64 +1,78 @@
-<li class="agenda-item">
-    <div class=" d-flex d-flex align-items-start">
+<li class="px-5 my-4">
+    <div class="d-flex d-flex align-items-start">
         <div class="agenda-item__date align-self-start">
-            <h5 class="agenda-item__date-day">{{ $activity->start_date->format('d') }}</h5>
+            <h5 class="agenda-item__date-day text-app-secondary">{{ $activity->start_date->format('d') }}</h5>
             <span class="agenda-item__date-month">{{ $activity->start_date->format('M') }}</span>
         </div>
 
-        <div class="agenda-item__body">
-            <a href="{{ action(
-                            [\Francken\Association\Activities\Http\ActivitiesController::class, 'show'] ,
-                            ['activity' => $activity]
+        <a href="{{ action(
+                        [\Francken\Association\Activities\Http\ActivitiesController::class, 'show'] ,
+                        ['activity' => $activity]
                         ) }}">
-                <h5 class="agenda-item__header">{{ $activity->name }}</h5>
-                <div class="d-flex align-items-center mt-2 mb-1">
-                    @if ($activity->signUpSettings !== null)
-                        @if ($activity->signUpSettings->is_free)
-                            <small class="text-muted">
-                                <i class="fas fa-euro-sign"></i>
-                                Free
-                            </small>
-                            @else
-                            <small class="text-muted font-weight-light">
-                                <i class="fas fa-euro-sign"></i>
-                                {{ number_format($activity->signUpSettings->costs_per_person / 100, 2) }}
-                            </small>
-                        @endif
-
-                            <small class="text-muted font-weight-light mx-2" style="font-size: 0.7rem">
-                                •
-                            </small>
-                        <small class="text-muted font-weight-light">
-                            <i class="fas fa-users mr-1"></i>
-
-                            {{ $activity->totalSignUps }} /
-
-                            @if ($activity->signUpSettings->max_sign_ups === null)
-                                ∞
-                            @else
-                                {{ $activity->signUpSettings->max_sign_ups }}
-                            @endif
-                        </small>
-                    @endif
-                    @if ($activity->signUpSettings !== null && $activity->comments_count > 0)
-                        <small class="text-muted font-weight-light mx-2" style="font-size: 0.7rem">
-                            •
-                        </small>
-                    @endif
-                    @if ($activity->comments_count > 0)
-                        <small class="text-muted font-weight-light">
-                            <i class="fas fa-comments"></i>
-                            {{ $activity->comments_count  }}
-                        </small>
+            <div class="text-app-secondary-dark d-flex justify-content-start mb-1">
+                <div class=" mr-1">
+                    @if ($activity->start_date->isToday())
+                        Today
+                    @elseif ($activity->start_date->isTomorrow())
+                        Tomorrow
+                    @else
+                        {{ $activity->start_date->format('l') }}
                     @endif
                 </div>
-                @if ($activity->signUpSettings !== null)
-                    <small class="text-muted font-weight-light">
-                        <i class="fas fa-clock"></i>
+                <div>
+                    - {{ $activity->start_date->format('H:i') }}
+                </div>
+            </div>
+            <h6 class="mb-0">{{ $activity->name }}</h6>
+            @if ($activity->signUpSettings !== null)
+                @if ($activity->signUpSettings->deadline_at->isCurrentWeek())
+                    <small class="text-muted">
+                        <i class="fas fa-clock text-primary"></i>
                         Registration ends in {{ $activity->signUpSettings->deadline_at->diffForHumans() }}
                     </small>
                 @endif
-            </a>
-        </div>
+            @endif
+            <div class="d-flex align-items-center mt-2 pt-1 mb-1">
+                @if ($activity->signUpSettings !== null)
+                    @if ($activity->signUpSettings->is_free)
+                        <small class="text-muted">
+                            <i class="fas fa-euro-sign"></i>
+                            Free
+                        </small>
+                    @else
+                        <small class="text-muted font-weight-light">
+                            <i class="fas fa-euro-sign"></i>
+                            {{ number_format($activity->signUpSettings->costs_per_person / 100, 2) }}
+                        </small>
+                    @endif
+
+                    <small class="text-muted font-weight-light mx-2" style="font-size: 0.7rem">
+                        •
+                    </small>
+                    <small class="text-muted font-weight-light">
+                        <i class="fas fa-users mr-1"></i>
+
+                        {{ $activity->totalSignUps }} /
+
+                        @if ($activity->signUpSettings->max_sign_ups === null)
+                            ∞
+                        @else
+                            {{ $activity->signUpSettings->max_sign_ups }}
+                        @endif
+                    </small>
+                @endif
+                @if ($activity->signUpSettings !== null && $activity->comments_count > 0)
+                    <small class="text-muted font-weight-light mx-2" style="font-size: 0.7rem">
+                        •
+                    </small>
+                @endif
+                @if ($activity->comments_count > 0)
+                    <small class="text-muted font-weight-light">
+                        <i class="fas fa-comments"></i>
+                        {{ $activity->comments_count  }}
+                    </small>
+                @endif
+            </div>
+        </a>
     </div>
 </li>
--- resources/views/homepage/_agenda.blade.php
+++ resources/views/homepage/_agenda.blade.php
@@ -1,12 +1,14 @@
-<div class="agenda">
-    <h3 class="section-header agenda-header">
-        Agenda
-    </h3>
+<div class="bg-white shadow py-5 mb-5">
+    <div class="px-5 mb-3">
+        <h3 class="section-header">
+            Agenda
+        </h3>
+    </div>
     <ul class="agenda-list list-unstyled">
         @foreach ($activities as $activity)
             @include('./homepage._agenda-item', ['activity' => $activity])
         @endforeach
-        <li class="agenda-item d-flex d-flex align-items-center">
+        <li class="agenda-item d-flex d-flex align-items-center px-5">
             <div class="agenda-item__date align-self-start">
                 <h5 class="agenda-item__date-day">
                     <i class="fa fa-calendar" aria-hidden="true"></i>
