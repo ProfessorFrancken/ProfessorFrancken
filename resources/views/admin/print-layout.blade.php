92b68af2c17406851ac5043e712a3f96054d55e3
Mark Redeman <markredeman@gmail.com>
2020-11-29 18:46:47 +0100

    Update fontawesome

--- resources/views/admin/print-layout.blade.php
+++ resources/views/admin/print-layout.blade.php
@@ -8,7 +8,7 @@
         @include('layout._seo')
         @include('layout._favicon')
 
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
         @unless(config('env') == 'testing')
             <link rel="stylesheet" href="{{ mix('/dist/css/print.css') }}">
         @endunless
