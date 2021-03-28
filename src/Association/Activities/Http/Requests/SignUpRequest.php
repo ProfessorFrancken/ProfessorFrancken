355ada8d8f490276be2b71324bcfc5d086dfc9ac
Mark Redeman <markredeman@gmail.com>
2021-03-28 20:39:48 +0200

    Fix SignUpRequest when no max sign ups is given

--- src/Association/Activities/Http/Requests/SignUpRequest.php
+++ src/Association/Activities/Http/Requests/SignUpRequest.php
@@ -17,7 +17,7 @@ class SignUpRequest extends FormRequest
     public function rules() : array
     {
         return [
-            'plus_ones' => ['nullable', 'integer', 'max:' . $this->maxPlusOnes()],
+            'plus_ones' => $this->plusOneRule(),
             'dietary_wishes' => ['nullable', 'min:1'],
             'has_drivers_license' => ['nullable', 'boolean'],
         ];
@@ -48,20 +48,27 @@ class SignUpRequest extends FormRequest
         ];
     }
 
-    private function maxPlusOnes() : int
+    private function plusOneRule() : array
     {
         $activity = $this->route('activity');
         Assert::isInstanceOf($activity, Activity::class);
         Assert::notNull($activity->signUpSettings);
 
-        $maxPlusOnes = (int)$activity->signUpSettings->max_sign_ups - $activity->total_sign_ups - 1;
+        $maxSignUps = $activity->signUpSettings->max_sign_ups;
+
+        if ($maxSignUps === null) {
+            return ['nullable', 'integer'];
+        }
+
+        // Make sure a member can't have more plus ones than the max total sign ups
+        $maxPlusOnes = (int)$maxSignUps - $activity->total_sign_ups - 1;
 
         // make sure the current sign up is not counted in the max
         $signUp = $this->route('sign_up');
         if ($signUp instanceof SignUp) {
-            return $maxPlusOnes + 1 + (int)$signUp->plus_ones;
+            $maxPlusOnes += 1 + (int)$signUp->plus_ones;
         }
 
-        return $maxPlusOnes;
+        return ['nullable', 'integer', 'max:' . $maxPlusOnes];
     }
 }
