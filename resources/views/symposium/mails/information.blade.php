24784c7f85030c1c17ca1acfeea3667dd39304ca
Mark Redeman <markredeman@gmail.com>
2021-06-27 13:56:59 +0200

    Update symposium information mail

--- resources/views/symposium/mails/information.blade.php
+++ resources/views/symposium/mails/information.blade.php
@@ -1,45 +1,37 @@
 @component('mail::layout')
     {{-- Header --}}
     @slot('header')
-        @include('symposium.mails._header', ['symposium' => $symposium]) @endslot
+        @include('symposium.mails._header', ['symposium' => $symposium])
+    @endslot
 
     {{-- Body --}}
-# Hi {{ $fullname }},
+Dear {{ $fullname }},
 
-Thank you once again for signing up for the Symposium 'In a materialistic world'.
-With this email we would like to inform you about the location, dresscode and schedule of the symposium.
+Thank you once again for signing up for the Symposium 'Cognitive Matters - Physics of cognitive advancements'. With this email we would like to inform you about the schedule of the symposium and the Zoom link.
 
-### The location: {{ $symposium->location }}
+### Zoom
 
-This year the Symposium is held at the <a href="{{ $symposium->location_google_maps_url }}" target="_blank">{{ $symposium->location }}</a>.
-Click the link below to get travel directions.
+This year the symposium will take place online using the platform Zoom. Click the link below to join the Zoom call. We kindly ask you to keep your microphone muted at all times, unless you have a question or comment of course.
 
-@component('mail::button', ['url' => $symposium->location_google_maps_url])
-    Travel directions to {{ $symposium->location }}
+@component('mail::button', ['url' =>  ''])
+    Link to Zoom
+@endcomponent
+
+### Schedule
+
+The Zoom call will open at 10:00 at Wednesday. The official program will start at 10:15 with a talk by our chair of the day, Prof. dr. Beatriz Noheda. If this doesn't suit you, you are of course welcome to join at any later moment. You can find more information about the program in the attached programme ().
+
+@component('mail::button', ['url' =>  'https://professorfrancken.nl/uploads/association/symposia/cognitive-matters-programme.pdf'])
+    Progamme booklet
 @endcomponent
 
 ### Colloquium points
 
-By visiting the symposium you can earn 1 physics colloquium point.
-The attendance list for the colloquium points can be found at the entrance of the venue.
+By visiting the symposium you can earn physics colloquium points. One colloquium point is rewarded for attending two talks and two colloquium points are rewarded for attending the whole day. Please email the committee if you want to make use of this, so that we can track your attendance.
 
-### Dresscode
+### Lunch and borrelbox
 
-The dresscode for the symposium is business casual.
-
-### Schedule
-
-The doors of the symposium open at 09:00.
-We hope to see you before the start of the official program at 9.25.
-If this doesn't suit you, you are of course welcome to join at any later moment.
-
-@component('mail::table')
-| Time          |               |
-| ------------- |:-------------|
-@foreach ($schedule as $slot)
-| <strong>{{ $slot['time'] }}</strong> | {{ $slot['title'] }} |
-@endforeach
-@endcomponent
+If you have ordered lunch and/or a borrelbox these will be delivered to your home. The borrelbox will be delivered today by the committee. The lunch will be delivered by Broodje Ben between 10:30 and 13:00 on the day of the symposium.
 
 @include('symposium.mails._footer', ['symposium' => $participant->symposium])
 
