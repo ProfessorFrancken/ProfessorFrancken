738e7526a4e7b2cdf39a43d0b07c9e364b61bf69
Mark Redeman <markredeman@gmail.com>
2021-10-12 14:07:31 +0200

    Show role's accounts

--- resources/views/admin/compucie/roles/show.blade.php
+++ resources/views/admin/compucie/roles/show.blade.php
@@ -75,7 +75,21 @@
         </div>
 
         <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                    <h3>Accounts with this role</h3>
 
+                    <ul>
+                        @foreach ($role->users as $account)
+                            <li>
+                                <a href={{ action([\Francken\Auth\Http\Controllers\Admin\AccountsController::class, 'show'], ['account' => $account])  }}>
+                                    {{ $account->member->fullname }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
