3844d2b6bc9cad1b5c34ebec49d4af5a02558a58
Mark Redeman <markredeman@gmail.com>
2023-09-12 23:21:52 +0200

    Apply global scopes to Photo and Album

--- src/Association/Photos/Album.php
+++ src/Association/Photos/Album.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace Francken\Association\Photos;
 
 use Francken\Association\Photos\Http\Controllers\PhotosController;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Gate;
 
 final class Album extends Model
 {
@@ -91,5 +94,36 @@ final class Album extends Model
     public function addView() : void
     {
         // Faking this impelementation for now
+    }
+
+    /**
+     * The "booted" method of the model.
+     */
+    protected static function booted() : void
+    {
+        static::addGlobalScope(new class() implements Scope {
+            /**
+             * Apply the scope to a given Eloquent query builder.
+             * @param Builder<Album> $builder
+             */
+            public function apply(
+                Builder $builder,
+                Model $model
+            ) : void {
+                $visibilities = [];
+                if (Gate::allows('view-private-albums')) {
+                    $visibilities[] = 'private';
+                }
+                if (Gate::allows('view-members-only-albums')) {
+                    $visibilities[] = 'members-only';
+                }
+                if (Gate::allows('view-albums')) {
+                    $visibilities[] = 'public';
+                }
+
+
+                $builder->whereIn('visibility', $visibilities);
+            }
+        });
     }
 }
--- src/Association/Photos/Photo.php
+++ src/Association/Photos/Photo.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Francken\Association\Photos;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Gate;
 
 final class Photo extends Model
 {
@@ -36,7 +39,7 @@ final class Photo extends Model
     /** @return BelongsTo<Album, Photo> */
     public function album() : BelongsTo
     {
-        return $this->belongsTo(Album::class);
+        return $this->belongsTo(Album::class, 'album_id');
     }
 
     public function src() : string
@@ -47,5 +50,35 @@ final class Photo extends Model
     public function getIsTallAttribute() : bool
     {
         return false;
+    }
+
+    /**
+     * The "booted" method of the model.
+     */
+    protected static function booted() : void
+    {
+        static::addGlobalScope(new class() implements Scope {
+            /**
+             * Apply the scope to a given Eloquent query builder.
+             * @param Builder<Photo> $builder
+             */
+            public function apply(
+                Builder $builder,
+                Model $model
+            ) : void {
+                $visibilities = [];
+                if (Gate::allows('view-private-albums')) {
+                    $visibilities[] = 'private';
+                }
+                if (Gate::allows('view-members-only-albums')) {
+                    $visibilities[] = 'members-only';
+                }
+                if (Gate::allows('view-albums')) {
+                    $visibilities[] = 'public';
+                }
+
+                $builder->whereIn('visibility', $visibilities);
+            }
+        });
     }
 }
