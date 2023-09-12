ae7eacb611b993276f2139c9fe7e215d9b6d872c
sjourdbeans <sjoerdbuitjes@gmail.com>
2023-09-12 17:14:26 +0200

    Changed intercie email and replaced dead url's
    The intercie email was outdated, and the some of the links in the page were dead. I replaced them, and also replaced some apostrofes because I didn't like the warning vscode gave me :)

--- resources/views/pages/study/internationals.blade.php
+++ resources/views/pages/study/internationals.blade.php
@@ -13,7 +13,7 @@ $breadcrumbs = [
         The following information and useful tips for students starting their studies in Groningen were collected by the <a href="/association/committees/intercie" class="font-weight-bold">intercie</a> committee.
     </p>
     <p>
-        If you have further questions feel free to contact them at <a href="mailto:intercietfv@gmail.com">intercietfv@gmail.com</a>.
+        If you have further questions feel free to contact them at <a href="mailto:interciefrancken@gmail.com">interciefrancken@gmail.com</a>.
     </p>
 
     <h2 id="bank-account" class="mb-1 mt-4">Bank account</h2>
@@ -32,21 +32,21 @@ $breadcrumbs = [
         You will receive a BSN number a couple of weeks after you applied.
     </p>
     <p class="text-justify">
-        You can take appointment here: <a href="https://gemeente.groningen.nl/moving-to-the-netherlands" class="font-weight-bold"'>https://gemeente.groningen.nl/moving-to-the-netherlands</a>.
+        You can take appointment here: <a href="https://gemeente.groningen.nl/en/moving-netherlands" class="font-weight-bold"'>https://gemeente.groningen.nl/en/moving-netherlands</a>.
     </p>
 
     <h2 id="housing" class="mb-1 mt-4">Housing information</h2>
     <p class="text-justify">
         We all know the struggles of finding a decent permanent place to stay in Groningen.
         The housing crisis is not getting any better and we are here to help!
-        First, you should check the <a href="https://www.rug.nl/education/international-students/living-in-the-netherlands/" class="font-weight-bold">university website on housing</a> as it refers to a few other useful sites and updates.
-        Furhtermore, you can sign up with lefier to get an affordable student room.
+        First, you should check the <a href="https://www.rug.nl/education/bachelor/international-students/study-in-the-netherlands-groningen/" class="font-weight-bold">university website on housing</a> as it refers to a few other useful sites and updates.
+        Furthermore, you can sign up with lefier to get an affordable student room.
         The agency fee is only 30 which is less than most agents charge.
         However, there is a waiting time so keep that in mind.
         The best time to find a place is around December/January.
     </p>
     <p class="text-justify">
-        You can check if you’re being overcharged by entering in your room’s information on this website: <a href="http://groningerstudentenbond.nl/en/bereken-of-je-te-veel-huur-betaalt/" class="font-weight-bold">http://groningerstudentenbond.nl/en/bereken-of-je-te-veel-huur-betaalt/</a>.
+        You can check if you're being overcharged by entering in your room's information on this website: <a href="https://groningerstudentenbond.nl/huurcheck/" class="font-weight-bold">https://groningerstudentenbond.nl/huurcheck/</a>.
         For urgent cases: contact one of us through the contact details posted to find a way to arrange another temporary place.
     </p>
 
@@ -110,8 +110,8 @@ $breadcrumbs = [
         You show them these details in a building located at <a href="https://www.google.com/maps/place/Kempkensberg+12,+9722+TB+Groningen/data=!4m2!3m1!1s0x47c82d54700bdff3:0xd6c015e2e4f2d074?ved=2ahUKEwiu_4mx5sPgAhUGU1AKHQneD6gQ8gEwAHoECAAQAQ" class="font-weight-bold">Kempkensberg 12, 9722 TB</a> and you shall find that you may not need to go every month, so you could go once and claim the last three months and receive a lot of money.
     </p>
     <p class="text-justify">
-        Please be aware that you can work if you are not from the eu but it’s a low amount of hours, you have to get special visa requirements and is generally seen as a big hassle, but it is possible.
-        You also can’t get any loans from the dutch government in the non eu citizen case even if you work.
+        Please be aware that you can work if you are not from the eu but it's a low amount of hours, you have to get special visa requirements and is generally seen as a big hassle, but it is possible.
+        You also can't get any loans from the dutch government in the non eu citizen case even if you work.
     </p>
     <p class="text-justify">
         Working 14 hours a week may not seem like much but it is, however people can and do actually finance their university by themselves like this.
@@ -133,10 +133,10 @@ $breadcrumbs = [
     <h2 id="bikes" class="mb-1 mt-4">Bikes</h2>
     <p class="text-justify">
         You can buy a bike from any bikeshop, Marktplaats or facebook groups or you can pay monthly for a <a href="https://swapfiets.nl/">swapfiet</a>.
-        Don’t buy a bike when someone offers you one for very cheap in the city centre, it’s probably stolen.
+        Don't buy a bike when someone offers you one for very cheap in the city centre, it's probably stolen.
         You should probably try to get one with with hand breaks because it is easier to use if you never tried using pedal brakes but it it not difficult getting used to it.
         Swapfiets can be more expensive but if some damage happens to the bike it will get replaced or fixed anywhere in the city for free.
-        Remember to always have lights if you’re cycling after dark because you could get fined if you don’t (and it's safer!), you can buy lights in most stores like HEMA, Flying Tiger or So Low.
+        Remember to always have lights if you're cycling after dark because you could get fined if you don't (and it's safer!), you can buy lights in most stores like HEMA, Flying Tiger or So Low.
         Watch out for crossings where all the bike traffic lights go green at the same time, you can recognize them by a white sign with green arrows in a square.
     </p>
 
@@ -149,8 +149,8 @@ $breadcrumbs = [
 
     <h2 id="traveling" class="mb-1 mt-4">Transportation</h2>
     <p class="text-justify">
-        It’s cheaper to buy an ov chipkaart compared to buying tickets at the train station please note you need a BSN number.
-        You can get the OV <a href="https://www.ov-chipkaart.nl/purchase-an-ov-chipkaart.htm" class="font-weight-bold">here</a>.
+        It's cheaper to buy an ov chipkaart compared to buying tickets at the train station please note you need a BSN number.
+        You can get a (personal) OV <a href="https://www.ov-chipkaart.nl/en/apply-for-personal-ov-chipcard" class="font-weight-bold">here</a>.
         A cheap alternative to travel across the country if you're booking in advance is <a href="https://www.vakantieveilingen.nl/winstpakker.html">vakantieveilingen.nl</a>.
     </p>
 @endsection
