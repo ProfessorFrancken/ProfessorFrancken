3b15e9c8d5a986bf434ad30d28137de2872c5c9d
Mark Redeman <markredeman@gmail.com>
2023-09-21 19:52:35 +0200

    Add route for showing photos from nextcloud

--- src/Association/Photos/Photo.php
+++ src/Association/Photos/Photo.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Francken\Association\Photos;
 
+use Francken\Association\Photos\Http\Controllers\PhotosController;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Gate;
 
 final class Photo extends Model
 {
@@ -36,16 +39,42 @@ final class Photo extends Model
     /** @return BelongsTo<Album, Photo> */
     public function album() : BelongsTo
     {
-        return $this->belongsTo(Album::class);
+        return $this->belongsTo(Album::class, 'album_id');
     }
 
     public function src() : string
     {
-        return '';
+        return action(
+            [PhotosController::class, 'showImage'],
+            ['album' => $this->album_id, 'photo' => $this]
+        );
     }
 
     public function getIsTallAttribute() : bool
     {
         return false;
+    }
+
+    /**
+     * The "booted" method of the model.
+     */
+    protected static function booted() : void
+    {
+        static::addGlobalScope(
+            'view-permission',
+            function (Builder $builder) : void {
+                $visibilities = ['public'];
+
+                if (Gate::allows('view-private-albums')) {
+                    $visibilities[] = 'private';
+                }
+
+                if (Gate::allows('view-members-only-albums')) {
+                    $visibilities[] = 'members-only';
+                }
+
+                $builder->whereIn('visibility', $visibilities);
+            }
+        );
     }
 }
