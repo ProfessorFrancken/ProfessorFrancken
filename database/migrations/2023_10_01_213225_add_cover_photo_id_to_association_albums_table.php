6cf7a0faea227ab9b7bf5a12a898651a71543d95
Mark Redeman <markredeman@gmail.com>
2023-10-01 21:37:39 +0200

    Add a migration for an album's cover photo

---
+++ database/migrations/2023_10_01_213225_add_cover_photo_id_to_association_albums_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('association_albums', function (Blueprint $table) {
+            $table->bigInteger('cover_photo_id')->unsigned()->nullable();
+            $table->foreign('cover_photo_id')->references('id')->on('association_photos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('association_albums', function (Blueprint $table) {
+            $table->dropForeign(['cover_photo_id']);
+            $table->dropColumn('cover_photo_id');
+        });
+    }
+};
